5ba6f9b18c87458c4650f482eebde973663582e5
Alexey Kalita <kalita.alexey@outlook.com>
2020-12-09 16:52:56 +0300

    Provide --ws-port

--- run_script.sh
+++ run_script.sh
@@ -32,18 +32,20 @@ function local_id() {
 }
 
 port="10000"
+wsport="9944"
 start1="1"
 for name in alice bob charlie dave eve
 do
 	newport=`expr $port + 1`
 	if [ "$start1" == "1" ]; then
-		sh -c "./target/release/framenode --tmp --$name --port $newport --chain local 2>&1" | local_id | tee $tmpdir/port_${newport}_name_$name.txt &
+		sh -c "./target/release/framenode --tmp --$name --port $newport --ws-port $wsport --chain local 2>&1" | local_id | tee $tmpdir/port_${newport}_name_$name.txt &
 	else
-		sh -c "./target/release/framenode --tmp --$name --port $newport --chain local --bootnodes /ip4/127.0.0.1/tcp/$port/p2p/`cat $localid` 2>&1" | local_id > $tmpdir/port_${newport}_name_$name.txt &
+		sh -c "./target/release/framenode --tmp --$name --port $newport --ws-port $wsport --chain local --bootnodes /ip4/127.0.0.1/tcp/$port/p2p/`cat $localid` 2>&1" | local_id > $tmpdir/port_${newport}_name_$name.txt &
 	fi
-	echo SCRIPT: $newport $port $name $tmpdir/port_${newport}_name_$name.txt
+	echo SCRIPT: $newport $port $name $wsport $tmpdir/port_${newport}_name_$name.txt
 	sleep 5
 	port="$newport"
+	wsport=`expr $wsport + 1`
 	start1="0"
 done
 
