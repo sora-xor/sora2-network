69f59004a6a501bf4f78e9a167f28ad357444e94
Evgeni Zhukov <zhukov@soramitsu.co.jp>
2020-12-10 16:27:22 +0700

    Updating run_script, macos compat, detecting gawk, using mktemp, using tee to output logs of first node, checking that framenode binary exist, solving problem with wait command, output logfiles paths in console adding missing scripts from substrate-node-template

--- run_script.sh
+++ run_script.sh
@@ -2,12 +2,27 @@
 
 export RUST_LOG="sc_rpc=trace"
 
+localid=`mktemp`
+tmpdir=`dirname $localid`
+
+if which gawk > /dev/null 2>&1; then
+	awk="gawk"
+else
+	awk="awk"
+fi
+
+if [ ! -f ./target/release/framenode ]; then
+	echo "Please build framenode binary"
+	echo "for example by running command: cargo build --release"
+	exit 1
+fi
+
 function local_id() {
-  awk "
+  $awk "
     BEGIN { a=1 }
     /Local node identity is: /{
       if (a) {
-        print \$10 > \"/tmp/localid.txt\";
+        print \$10 > \"$localid\";
         fflush();
         a=0
       }
@@ -22,20 +37,20 @@ for name in alice bob charlie dave eve
 do
 	newport=`expr $port + 1`
 	if [ "$start1" == "1" ]; then
-		sh -c "./target/release/framenode --tmp --$name --port $newport --chain local 2>&1" | local_id > /tmp/port_${newport}_name_$name.txt &
+		sh -c "./target/release/framenode --tmp --$name --port $newport --chain local 2>&1" | local_id | tee $tmpdir/port_${newport}_name_$name.txt &
 	else
-		sh -c "./target/release/framenode --tmp --$name --port $newport --chain local --bootnodes /ip4/127.0.0.1/tcp/$port/p2p/`cat /tmp/localid.txt` 2>&1" | local_id > /tmp/port_${newport}_name_$name.txt &
+		sh -c "./target/release/framenode --tmp --$name --port $newport --chain local --bootnodes /ip4/127.0.0.1/tcp/$port/p2p/`cat $localid` 2>&1" | local_id > $tmpdir/port_${newport}_name_$name.txt &
 	fi
-	echo $newport $port $name
+	echo SCRIPT: $newport $port $name $tmpdir/port_${newport}_name_$name.txt
 	sleep 5
 	port="$newport"
 	start1="0"
 done
 
-echo test
-
-#sleep 900
-
-#pkill -KILL framenode
-
 wait
+
+echo SCRIPT: you can stop script by control-C hot key
+echo SCRIPT: maybe framenode processes is still runnning, you can check it and finish it by hand
+echo SCRIPT: in future this can be done automatically
+
+sleep 999999
