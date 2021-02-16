83ab75969541d5339ac2bd176c12f9c71248eb20
Evgeni Zhukov <zhukov@soramitsu.co.jp>
2021-02-16 16:36:45 +0700

    [PSS-435] Using with_transaction
    Update shell.nix to use modern rust compiler
    
    Using with_transaction in can_exchange
    
    Using with_transaction in quote
    
    Fix tests
    
    Requested PR changes

--- misc/shell.nix
+++ misc/shell.nix
@@ -1,4 +1,36 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [ protobuf llvm clang pkgconfig openssl ];
+let
+
+  mozillaOverlay =
+    import (builtins.fetchGit {
+      url = "https://github.com/mozilla/nixpkgs-mozilla.git";
+      rev = "18cd4300e9bf61c7b8b372f07af827f6ddc835bb";
+    });
+
+  nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
+
+  rust-nightly = with nixpkgs; ((rustChannelOf { date = "2021-02-11"; channel = "nightly"; }).rust.override {
+    targets = [ "wasm32-unknown-unknown" ];
+    extensions = ["rust-src"];
+  });
+
+in
+with nixpkgs; pkgs.mkShell {
+  buildInputs = [
+
+    git
+
+    clang
+    cmake
+    pkg-config
+    rust-nightly
+
+  ] ++ stdenv.lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.Security
+  ];
+
+  LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
+  PROTOC = "${protobuf}/bin/protoc";
+  ROCKSDB_LIB_DIR = "${rocksdb}/lib";
+
 }
+
