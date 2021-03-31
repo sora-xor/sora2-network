9adab80ee06388ab3b2aabd4a5425a959b46da5a
Eugene Kovalev <eugene.a.kovalev@gmail.com>
2021-03-31 13:23:20 +0300

    Bump spec_version
    Signed-off-by: Eugene Kovalev <eugene.a.kovalev@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 6,
+    spec_version: 7,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
