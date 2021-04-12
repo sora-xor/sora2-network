4344ec145e5e8a2f73205ade7568187f7bca9978
武宮誠 <takemiya@soramitsu.co.jp>
2021-04-12 15:34:09 +0400

    update math for token bonding curve strategic bonuses

--- pallets/multicollateral-bonding-curve-pool/src/lib.rs
+++ pallets/multicollateral-bonding-curve-pool/src/lib.rs
@@ -1138,7 +1138,7 @@ impl<T: Config> Module<T> {
         let actual_before: FixedWrapper =
             Self::actual_reserves_reference_price(reserves_account_id, collateral_asset_id)?.into();
         // let incentivised_currencies_num: u128 = IncentivisedCurrenciesNum::<T>::get().into();
-        let N: FixedWrapper = FixedWrapper::from(7); //TODO: fix this at 7 for launch //incentivised_currencies_num * balance!(1));
+        let N: FixedWrapper = FixedWrapper::from(balance!(10)); //TODO: fix this at 10 for launch //incentivised_currencies_num * balance!(1));
         let P: FixedWrapper = FixedWrapper::from(InitialPswapRewardsSupply::<T>::get());
 
         // Calculate reward.
