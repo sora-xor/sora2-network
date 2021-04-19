72522200f9c416dbc10d5a876fa2a9cf2cdb15be
Maksim Surkov <modbrin@gmail.com>
2021-04-19 13:22:12 +0300

    Bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -192,7 +192,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 27,
+    spec_version: 28,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
