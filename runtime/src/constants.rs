029a6665fc2edc029231d129e3bec668e31acdec
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-07-15 20:13:40 +0300

    change hours
    Signed-off-by: Stepan Lavrentev <lawrentievsv@gmail.com>

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -64,7 +64,7 @@ pub mod time {
 
     // These time units are defined in number of blocks.
     pub const MINUTES: BlockNumber = 60 / (SECS_PER_BLOCK as BlockNumber);
-    pub const HOURS: BlockNumber = MINUTES * 10;
+    pub const HOURS: BlockNumber = MINUTES * 2;
     pub const DAYS: BlockNumber = HOURS * 24;
 }
 
