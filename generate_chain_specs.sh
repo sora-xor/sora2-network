5b59522b3528f19c794be66dd1fbdd16caca1791
Vladislav <negigic@gmail.com>
2021-09-21 16:32:31 +0300

    generate_chain_specs.sh arguments (#517)
    Signed-off-by: Vladislav Markushin <negigic@gmail.com>

--- generate_chain_specs.sh
+++ generate_chain_specs.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cargo run --bin framenode --release --features include-real-files -- build-spec --chain main-coded --raw > node/chain_spec/src/bytes/chain_spec_main.json
-cargo run --bin framenode --release --features "private-net include-real-files reduced-pswap-reward-periods" -- build-spec --chain test-coded --raw > node/chain_spec/src/bytes/chain_spec_test.json
-cargo run --bin framenode --release --features "private-net include-real-files" -- build-spec --chain staging-coded --raw > node/chain_spec/src/bytes/chain_spec_staging.json
+arg=$1
+
+case $arg in
+  [mM] ) cargo run --bin framenode --release --features include-real-files -- build-spec --chain main-coded --raw > node/chain_spec/src/bytes/chain_spec_main.json;;
+  [tT] ) cargo run --bin framenode --release --features "private-net include-real-files reduced-pswap-reward-periods" -- build-spec --chain test-coded --raw > node/chain_spec/src/bytes/chain_spec_test.json;;
+  [sS] ) cargo run --bin framenode --release --features "private-net include-real-files" -- build-spec --chain staging-coded --raw > node/chain_spec/src/bytes/chain_spec_staging.json;;
+  [nN] ) ;;
+  * ) echo "Please provide network. t - test, s - stage, m - master";;
+esac
