78c3d0f71e403bfe9c489f8f1fed4afbef6d36e8
Vladislav Markushin <negigic@gmail.com>
2021-11-09 12:12:48 +0300

    Bump VERSION

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -206,10 +206,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 13,
+    spec_version: 14,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 13,
+    transaction_version: 14,
 };
 
 /// The version infromation used to identify this runtime when compiled natively.
