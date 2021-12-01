188de27c74a6f1bc2f418633469ce8660a6e1de5
Vladimir Stepanenko <vovac12@gmail.com>
2021-12-01 23:31:54 +0300

    Change report file name

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -9,4 +9,4 @@ export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
 cargo test --features private-net
 
-grcov . --binary-path target/debug -s . -t cobertura --branch --ignore-not-existing -o target/debug/report
+grcov . --binary-path target/debug -s . -t cobertura --branch --ignore-not-existing -o target/debug/cobertura_report
