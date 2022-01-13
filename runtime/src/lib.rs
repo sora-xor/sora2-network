aefd5d67acc521d21c0d7664388fd056c88061fb
Vladimir Stepanenko <vovac12@gmail.com>
2022-01-13 16:09:25 +0300

    Fix AllPallets

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1912,7 +1912,7 @@ pub type Executive = frame_executive::Executive<
     Block,
     frame_system::ChainContext<Runtime>,
     Runtime,
-    AllPallets,
+    AllPalletsWithSystem,
     MigratePalletVersionToStorageVersion,
 >;
 
