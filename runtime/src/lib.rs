d303457a78b4797e7beebb3f73284e331705196a
Alexey Kalita <kalita.alexey@outlook.com>
2022-02-01 20:37:34 +0300

    1.3.1

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -216,10 +216,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 22,
+    spec_version: 23,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 22,
+    transaction_version: 23,
 };
 
 /// The version infromation used to identify this runtime when compiled natively.
