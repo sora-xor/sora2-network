cf81fff9b126140768322c885e61f3da97912b2a
Alexey <kalita.alexey@outlook.com>
2022-05-17 14:39:00 +0300

    release version 35 (#663)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -219,10 +219,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 34,
+    spec_version: 35,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 34,
+    transaction_version: 35,
 };
 
 /// The version infromation used to identify this runtime when compiled natively.
