d433b745d15e4a00ebd9704595968594a491a489
Vladimir Stepanenko <vovac12@gmail.com>
2022-05-24 15:16:51 +0300

    Update coverage.sh

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -7,6 +7,6 @@ export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
-cargo test --features private-net
+#cargo test --features private-net
 
-grcov . --binary-path target/debug -s . -t cobertura --branch --ignore-not-existing -o ./cobertura_report
+#grcov . --binary-path target/debug -s . -t cobertura --branch --ignore-not-existing -o ./cobertura_report
