aa4e82b4e21fd65a2e004a6dc2c23ab0936cebdb
Vladimir Stepanenko <vovac12@gmail.com>
2022-05-24 13:02:08 +0300

    Use stable rust in coverage

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -3,10 +3,10 @@
 cargo install grcov
 rustup component add llvm-tools-preview
 
-export RUSTFLAGS="-Zinstrument-coverage"
+export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
-cargo +nightly test --features private-net
+cargo test --features private-net
 
 grcov . --binary-path target/debug -s . -t cobertura --branch --ignore-not-existing -o ./cobertura_report
