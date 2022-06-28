be6a112a410624b7011fe4d7c3616298ec6e5103
Alexey <kalita.alexey@outlook.com>
2022-06-28 13:26:40 +0300

    Set version to 1.4.4 (#691)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -218,10 +218,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 35,
+    spec_version: 36,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 35,
+    transaction_version: 36,
 };
 
 /// The version infromation used to identify this runtime when compiled natively.
