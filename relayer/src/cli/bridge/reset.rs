89afddc47d356da22fff894c9b37d2028d988206
Vladimir Stepanenko <vovac12@gmail.com>
2022-06-28 12:27:03 +0300

    Remove debug logs and reset to latest commitment

--- relayer/src/cli/bridge/reset.rs
+++ relayer/src/cli/bridge/reset.rs
@@ -14,55 +14,38 @@ pub(crate) struct Command {
 
 impl Command {
     pub(super) async fn run(&self) -> AnyResult<()> {
-        println!("{}:{}", file!(), line!());
         let eth = self.eth.get_signed_ethereum().await?;
-        println!("{}:{}", file!(), line!());
         let sub = self.sub.get_unsigned_substrate().await?;
-        println!("{}:{}", file!(), line!());
-        let block_hash = sub.block_hash(Some(1u32)).await?;
-        println!("{}:{}", file!(), line!());
-        let autorities = sub
-            .api()
-            .storage()
-            .mmr_leaf()
-            .beefy_next_authorities(false, Some(block_hash))
-            .await?;
-        println!("{}:{}", file!(), line!());
         let eth_app = ethereum_gen::ETHApp::new(self.eth_app.clone(), eth.inner());
-        println!("{}:{}", file!(), line!());
         let (basic_inbound_address, basic_outbound_address) = eth_app.channels(0).call().await?;
-        println!("{}:{}", file!(), line!());
         let (incentivized_inbound_address, incentivized_outbound_address) =
             eth_app.channels(1).call().await?;
-        println!("{}:{}", file!(), line!());
         let basic_inbound =
             ethereum_gen::BasicInboundChannel::new(basic_inbound_address, eth.inner());
-        println!("{}:{}", file!(), line!());
         let basic_outbound =
             ethereum_gen::BasicOutboundChannel::new(basic_outbound_address, eth.inner());
-        println!("{}:{}", file!(), line!());
         let incentivized_inbound = ethereum_gen::IncentivizedInboundChannel::new(
             incentivized_inbound_address,
             eth.inner(),
         );
-        println!("{}:{}", file!(), line!());
         let incentivized_outbound = ethereum_gen::IncentivizedOutboundChannel::new(
             incentivized_outbound_address,
             eth.inner(),
         );
-        println!("{}:{}", file!(), line!());
         let beefy_address = basic_inbound.beefy_light_client().call().await?;
-        println!("{}:{}", file!(), line!());
         let beefy = ethereum_gen::BeefyLightClient::new(beefy_address, eth.inner());
-        println!("{}:{}", file!(), line!());
         let validator_registry_address = beefy.validator_registry().call().await?;
-        println!("{}:{}", file!(), line!());
         let validator_registry =
             ethereum_gen::ValidatorRegistry::new(validator_registry_address, eth.inner());
-        println!("{}:{}", file!(), line!());
         let registry_owner = validator_registry.owner().call().await?;
-        println!("{}:{}", file!(), line!());
         if registry_owner == eth.address() {
+            let block_hash = sub.block_hash(Some(1u32)).await?;
+            let autorities = sub
+                .api()
+                .storage()
+                .mmr_leaf()
+                .beefy_next_authorities(false, Some(block_hash))
+                .await?;
             info!("Updating validator registry");
             let call: ContractCall<_, _> =
                 validator_registry.update(autorities.root.0, autorities.len.into(), autorities.id);
@@ -86,9 +69,21 @@ impl Command {
             let result = pending.await?;
             debug!("Confirmed: {:?}", result);
         } else if registry_owner == beefy_address && beefy.owner().call().await? == eth.address() {
+            let block_number = sub.block_number::<u32>(None).await?;
+            let block_hash = sub.block_hash(Some(1u32)).await?;
+            let autorities = sub
+                .api()
+                .storage()
+                .mmr_leaf()
+                .beefy_next_authorities(false, Some(block_hash))
+                .await?;
             info!("Reset beefy contract");
-            let call: ContractCall<_, _> =
-                beefy.reset(0, autorities.root.0, autorities.len.into(), autorities.id);
+            let call: ContractCall<_, _> = beefy.reset(
+                block_number as u64,
+                autorities.root.0,
+                autorities.len.into(),
+                autorities.id,
+            );
             let call = call.legacy().from(eth.address());
             debug!("Static call: {:?}", call);
             call.call().await?;
