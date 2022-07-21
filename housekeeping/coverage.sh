2958433f688f40f3cab014186b1bb7aa5cff7b9c
Vladimir Stepanenko <vovac12@gmail.com>
2022-07-21 11:48:41 +0300

    Fix coverage

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -6,4 +6,4 @@ export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
 mold --run cargo test --features private-net
 
-grcov . --binary-path /app/target/debug -s . -t cobertura --branch --ignore-not-existing -o ./cobertura_report
+grcov . --binary-path ./target/debug -s . -t cobertura --branch --ignore-not-existing -o ./cobertura_report
