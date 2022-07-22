56bd007723d6e9e852b742aa5c0400754e63e445
Vladimir Stepanenko <vovac12@gmail.com>
2022-07-22 06:09:09 +0300

    Fix CI

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -4,6 +4,6 @@ export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
-mold --run cargo test --features private-net
+cargo test --features private-net
 
 grcov . --binary-path ./target/debug -s . -t cobertura --branch --ignore-not-existing -o ./cobertura_report
