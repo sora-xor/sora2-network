a62b5b7cda56041daa5260fdba590ca74767bc4b
Vladimir Stepanenko <vovac12@gmail.com>
2022-08-03 05:13:17 +0300

    Fix reset command

--- relayer/src/cli/bridge/reset.rs
+++ relayer/src/cli/bridge/reset.rs
@@ -17,22 +17,13 @@ impl Command {
         let eth = self.eth.get_signed_ethereum().await?;
         let sub = self.sub.get_unsigned_substrate().await?;
         let eth_app = ethereum_gen::ETHApp::new(self.eth_app.clone(), eth.inner());
-        let (basic_inbound_address, basic_outbound_address) = eth_app.channels(0).call().await?;
-        let (incentivized_inbound_address, incentivized_outbound_address) =
-            eth_app.channels(1).call().await?;
-        let basic_inbound =
-            ethereum_gen::BasicInboundChannel::new(basic_inbound_address, eth.inner());
-        let basic_outbound =
-            ethereum_gen::BasicOutboundChannel::new(basic_outbound_address, eth.inner());
-        let incentivized_inbound = ethereum_gen::IncentivizedInboundChannel::new(
-            incentivized_inbound_address,
-            eth.inner(),
-        );
-        let incentivized_outbound = ethereum_gen::IncentivizedOutboundChannel::new(
-            incentivized_outbound_address,
-            eth.inner(),
-        );
-        let beefy_address = basic_inbound.beefy_light_client().call().await?;
+        let inbound_channel_address = eth_app.inbound().call().await?;
+        let outbound_channel_address = eth_app.outbound().call().await?;
+        let inbound_channel =
+            ethereum_gen::InboundChannel::new(inbound_channel_address, eth.inner());
+        let outbound_channel =
+            ethereum_gen::OutboundChannel::new(outbound_channel_address, eth.inner());
+        let beefy_address = inbound_channel.beefy_light_client().call().await?;
         let beefy = ethereum_gen::BeefyLightClient::new(beefy_address, eth.inner());
         let validator_registry_address = beefy.validator_registry().call().await?;
         let validator_registry =
@@ -93,12 +84,7 @@ impl Command {
             let result = pending.await?;
             debug!("Confirmed: {:?}", result);
 
-            for call in [
-                basic_inbound.reset(),
-                basic_outbound.reset(),
-                incentivized_inbound.reset(),
-                incentivized_outbound.reset(),
-            ] {
+            for call in [inbound_channel.reset(), outbound_channel.reset()] {
                 info!("Reset {:?}", call.tx.to());
                 let call = call.legacy().from(eth.address());
                 debug!("Static call: {:?}", call);
