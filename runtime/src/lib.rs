4db66591504d2160d74240fcbe7cfa7eacd952eb
Vladimir Stepanenko <vovac12@gmail.com>
2022-10-21 16:47:48 +0300

    Update spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -218,10 +218,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 40,
+    spec_version: 41,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 40,
+    transaction_version: 41,
 };
 
 /// The version infromation used to identify this runtime when compiled natively.
