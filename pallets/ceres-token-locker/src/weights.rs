5a2e2e8e9279a591ee58a834eb642220c70a9927
Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>
2022-11-16 04:15:54 +0600

    ceres-token-locker
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>

--- pallets/ceres-token-locker/src/weights.rs
+++ pallets/ceres-token-locker/src/weights.rs
@@ -8,25 +8,19 @@ pub struct WeightInfo<T>(PhantomData<T>);
 
 impl<T: frame_system::Config> crate::WeightInfo for WeightInfo<T> {
     fn lock_tokens() -> Weight {
-        (129_400_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(9 as Weight))
-            .saturating_add(T::DbWeight::get().writes(6 as Weight))
+        Weight::zero()
     }
     fn withdraw_tokens() -> Weight {
-        (86_900_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(5 as Weight))
-            .saturating_add(T::DbWeight::get().writes(4 as Weight))
+        Weight::zero()
     }
     fn change_fee() -> Weight {
-        (28_000_000 as Weight)
-            .saturating_add(T::DbWeight::get().reads(1 as Weight))
-            .saturating_add(T::DbWeight::get().writes(1 as Weight))
+        Weight::zero()
     }
 }
 
 impl crate::WeightInfo for () {
     fn lock_tokens() -> Weight {
-        2 * EXTRINSIC_FIXED_WEIGHT
+        EXTRINSIC_FIXED_WEIGHT.mul(2)
     }
     fn withdraw_tokens() -> Weight {
         EXTRINSIC_FIXED_WEIGHT
