fbcba05081aa6911e427e8035d2ec1ecf0778b7e
Vladimir Stepanenko <vovac12@gmail.com>
2023-01-13 12:53:21 +0300

    Prepare for release (#858)

--- runtime/src/migrations.rs
+++ runtime/src/migrations.rs
@@ -3,21 +3,10 @@ use frame_support::traits::OnRuntimeUpgrade;
 use vested_rewards::migrations::MoveMarketMakerRewardPoolToLiquidityProviderPool;
 
 pub type Migrations = (
-    EthBridgeMigration,
     MoveMarketMakerRewardPoolToLiquidityProviderPool<Runtime>,
     PriceToolsMigration,
     pallet_staking::migrations::lock_fix::LockFix<Runtime>,
 );
-
-pub struct EthBridgeMigration;
-
-impl OnRuntimeUpgrade for EthBridgeMigration {
-    fn on_runtime_upgrade() -> Weight {
-        frame_support::log::warn!("Run migration EthBridgeMigration");
-        eth_bridge::migration::migrate::<Runtime>();
-        <Runtime as frame_system::Config>::BlockWeights::get().max_block
-    }
-}
 
 pub struct PriceToolsMigration;
 
