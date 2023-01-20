33e33740d27de5bf0ea53dd5aa99ed0e219b0189
Nikita Smirnov <neket2000@gmail.com>
2023-01-20 15:38:53 +0300

    Refactor/235 (#870)
    * added error field in event FeesExchangeFailed
    
    * extracted method calculate_and_burn_distribution
    
    * added docs
    
    * added flushing for cache files during coverage
    
    * added tests based on coverage
    
    Co-authored-by: Nikita Smirnov <nikita.smirnov@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -7,4 +7,4 @@ export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 cargo test --features private-net
 
 grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
-
+find . -type f -name '*.profraw' -delete
