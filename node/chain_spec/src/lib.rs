f1d88d4fefee4e2c885a8cca4a4d2e9c77c49cd6
Nikita Smirnov <neket2000@gmail.com>
2023-04-12 23:17:39 +0300

    quickfix

--- node/chain_spec/src/lib.rs
+++ node/chain_spec/src/lib.rs
@@ -2180,6 +2180,7 @@ fn mainnet_genesis(
                 (mbc_pool_rewards_account_id.clone(), 0),
                 (mbc_pool_free_reserves_account_id.clone(), 0),
                 (market_maker_rewards_account_id.clone(), 0),
+                (xst_pool_permissioned_account_id.clone(), 0),
             ]
             .into_iter()
             .chain(
