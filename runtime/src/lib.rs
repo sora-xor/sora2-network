014394a71002b0ee282a7d89f5bf0978b9ae422c
Vladimir Stepanenko <vovac12@gmail.com>
2023-04-20 15:38:02 +0300

    Update spec version to 52

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -250,10 +250,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 51,
+    spec_version: 52,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 51,
+    transaction_version: 52,
     state_version: 0,
 };
 
