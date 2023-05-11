a3c970b9a791ed54d43a3af9568f38bae59f9b68
Nikita Smirnov <neket2000@gmail.com>
2023-05-11 13:16:49 +0300

    added migrations to the migration list

--- runtime/src/migrations.rs
+++ runtime/src/migrations.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use common::{DEXId, XOR, XST, XSTUSD};
 
 pub struct StakingMigrationV11OldPallet;
 impl Get<&'static str> for StakingMigrationV11OldPallet {
@@ -11,6 +12,47 @@ pub struct EmptyAccountList;
 impl Get<Vec<AccountId>> for EmptyAccountList {
     fn get() -> Vec<AccountId> {
         Default::default()
+    }
+}
+
+pub struct XYKSyntheticPoolPairs<T>(core::marker::PhantomData<T>);
+
+impl<T> Get<Vec<(T::AssetId, T::AssetId, T::DEXId)>> for XYKSyntheticPoolPairs<T>
+where
+    T: assets::Config + common::Config,
+{
+    fn get() -> Vec<(T::AssetId, T::AssetId, T::DEXId)> {
+        vec![
+            (XSTUSD.into(), XST.into(), DEXId::PolkaswapXSTUSD.into()),
+            (XSTUSD.into(), XOR.into(), DEXId::PolkaswapXSTUSD.into()),
+            (XOR.into(), XSTUSD.into(), DEXId::Polkaswap.into()),
+        ]
+    }
+}
+
+pub struct XYKSyntheticPoolAccountList<T>(core::marker::PhantomData<T>);
+
+impl<T> Get<Vec<(T::AccountId, T::AccountId)>> for XYKSyntheticPoolAccountList<T>
+where
+    T: pool_xyk::Config,
+{
+    fn get() -> Vec<(T::AccountId, T::AccountId)> {
+        XYKSyntheticPoolPairs::<T>::get()
+            .iter()
+            .map(|(base_asset, target_asset, _)| {
+                pool_xyk::Properties::<T>::get(base_asset, target_asset)
+            })
+            .filter(|v| v.is_some())
+            .map(|v| v.unwrap())
+            .collect()
+    }
+}
+
+pub struct FarmingPoolBlocksToInspect;
+
+impl Get<Vec<BlockNumber>> for FarmingPoolBlocksToInspect {
+    fn get() -> Vec<BlockNumber> {
+        vec![230, 362, 501, 550, 632, 832, 1025]
     }
 }
 
@@ -29,4 +71,11 @@ pub type Migrations = (
     // We don't need this migration, so pass empty account list
     pallet_balances::migration::MigrateManyToTrackInactive<Runtime, EmptyAccountList>,
     multicollateral_bonding_curve_pool::migrations::v3::MigrateToV3<Runtime>,
+    farming::migrations::v3::Migrate<
+        Runtime,
+        XYKSyntheticPoolAccountList<Runtime>,
+        FarmingPoolBlocksToInspect,
+    >,
+    pswap_distribution::migrations::v2::Migrate<Runtime, XYKSyntheticPoolAccountList<Runtime>>,
+    pool_xyk::migrations::v3::XYKPoolUpgrade<Runtime, XYKSyntheticPoolPairs<Runtime>>,
 );
