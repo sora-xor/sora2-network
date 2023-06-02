db2ab59bd740649f1f41b353b14ca7d100ccd3c6
Vladimir Stepanenko <vovac12@gmail.com>
2023-06-02 18:51:23 +0600

    Fix migrations

--- runtime/src/migrations.rs
+++ runtime/src/migrations.rs
@@ -1,20 +1,6 @@
 use crate::*;
 use common::{DEXId, XOR, XST, XSTUSD};
 
-pub struct StakingMigrationV11OldPallet;
-impl Get<&'static str> for StakingMigrationV11OldPallet {
-    fn get() -> &'static str {
-        "BagsList"
-    }
-}
-pub struct EmptyAccountList;
-
-impl Get<Vec<AccountId>> for EmptyAccountList {
-    fn get() -> Vec<AccountId> {
-        Default::default()
-    }
-}
-
 pub struct XYKSyntheticPoolPairs<T>(core::marker::PhantomData<T>);
 
 impl<T> Get<Vec<(T::AssetId, T::AssetId, T::DEXId)>> for XYKSyntheticPoolPairs<T>
@@ -58,20 +44,6 @@ impl Get<Vec<BlockNumber>> for FarmingPoolBlocksToInspect {
 }
 
 pub type Migrations = (
-    pallet_staking::migrations::v10::MigrateToV10<Runtime>,
-    pallet_staking::migrations::v11::MigrateToV11<Runtime, BagsList, StakingMigrationV11OldPallet>,
-    pallet_staking::migrations::v12::MigrateToV12<Runtime>,
-    pallet_preimage::migration::v1::Migration<Runtime>,
-    pallet_scheduler::migration::v3::MigrateToV4<Runtime>,
-    pallet_democracy::migrations::v1::Migration<Runtime>,
-    pallet_multisig::migrations::v1::MigrateToV1<Runtime>,
-    pallet_scheduler::migration::v4::CleanupAgendas<Runtime>,
-    pallet_grandpa::migrations::CleanupSetIdSessionMap<Runtime>,
-    pallet_staking::migrations::v13::MigrateToV13<Runtime>,
-    pallet_election_provider_multi_phase::migrations::v1::MigrateToV1<Runtime>,
-    // We don't need this migration, so pass empty account list
-    pallet_balances::migration::MigrateManyToTrackInactive<Runtime, EmptyAccountList>,
-    multicollateral_bonding_curve_pool::migrations::v3::MigrateToV3<Runtime>,
     xst::migrations::CustomSyntheticsUpgrade<Runtime>,
     farming::migrations::v3::Migrate<
         Runtime,
