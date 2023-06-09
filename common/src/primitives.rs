54e11468a82ab66b4d283fd390127961463dc5f9
Vladimir Stepanenko <vovac12@gmail.com>
2023-06-09 16:10:23 +0300

    Add BoundedString type

--- common/src/primitives.rs
+++ common/src/primitives.rs
@@ -34,7 +34,10 @@ use core::fmt::Debug;
 use frame_support::dispatch::DispatchError;
 use frame_support::{ensure, RuntimeDebug};
 use hex_literal::hex;
+use scale_info::TypeInfo;
 use sp_core::H256;
+use sp_runtime::traits::Get;
+use sp_runtime::BoundedVec;
 use sp_std::convert::TryFrom;
 use sp_std::marker::PhantomData;
 use sp_std::vec::Vec;
@@ -1047,4 +1050,47 @@ mod tests {
 pub enum PriceVariant {
     Buy,
     Sell,
+}
+
+#[derive(Encode, MaxEncodedLen, Default, TypeInfo)]
+#[scale_info(skip_type_params(N))]
+pub struct BoundedString<N: Get<u32>>(BoundedVec<u8, N>);
+
+impl<N: Get<u32>> codec::Decode for BoundedString<N> {
+    fn decode<I: codec::Input>(input: &mut I) -> Result<Self, codec::Error> {
+        let inner = BoundedVec::<u8, N>::decode(input)?;
+        core::str::from_utf8(&inner).map_err(|_| "Invalid UTF-8 string")?;
+        Ok(Self(inner))
+    }
+}
+
+impl<N: Get<u32>> Clone for BoundedString<N> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<N: Get<u32>> PartialEq for BoundedString<N> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<N: Get<u32>> Eq for BoundedString<N> {}
+
+impl<N: Get<u32>> Debug for BoundedString<N> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        if let Ok(s) = core::str::from_utf8(&self.0) {
+            write!(f, "{:?}", s)
+        } else {
+            write!(f, "<invalid utf8 string>")
+        }
+    }
+}
+
+impl<N: Get<u32>> TryFrom<&str> for BoundedString<N> {
+    type Error = ();
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(Self(value.as_bytes().to_vec().try_into()?))
+    }
 }
