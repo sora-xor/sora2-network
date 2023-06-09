1b33597ab567cbb2a928d8b3fe49bf487551c489
Nikita Smirnov <neket2000@gmail.com>
2023-06-09 13:34:33 +0300

    update fee calc for swap_transfer_batch
    Co-authored-by: Vladimir Stepanenko <vovac12@gmail.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1206,7 +1206,7 @@ impl rewards::Config for Runtime {
 impl<T> xor_fee::ApplyCustomFees<RuntimeCall> for xor_fee::Pallet<T> {
     fn compute_fee(call: &RuntimeCall) -> Option<Balance> {
         let result = match call {
-            RuntimeCall::LiquidityProxy(liquidity_proxy::Call::swap_transfer_batch { .. }) => None,
+            RuntimeCall::LiquidityProxy(liquidity_proxy::Call::swap_transfer_batch { swap_batches, .. }) => swap_batches.iter().map(|x| x.receivers.len()).sum() * SMALL_FEE,
             RuntimeCall::Assets(assets::Call::register { .. })
             | RuntimeCall::EthBridge(eth_bridge::Call::transfer_to_sidechain { .. })
             | RuntimeCall::PoolXYK(pool_xyk::Call::withdraw_liquidity { .. })
