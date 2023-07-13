3d888d84cddada796db8c81fe5e043116aa2e8ca
bragov4ik <bragov4ik@hotmail.com>
2023-07-13 10:28:12 +0300

    rename runtime param

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1588,14 +1588,14 @@ impl faucet::Config for Runtime {
 }
 
 parameter_types! {
-    pub OrderBookOrderLifespan: Moment = 60*10*1000; // 10 minutes
+    pub QaToolsOrderLifespan: Moment = 60*10*1000; // 10 minutes
 }
 
 #[cfg(all(feature = "private-net", feature = "ready-to-test"))] // order-book
 impl qa_tools::Config for Runtime {
     type AssetInfoProvider = Assets;
     type WeightInfo = qa_tools::weights::SubstrateWeight<Runtime>;
-    type OrderBookOrderLifespan = OrderBookOrderLifespan;
+    type OrderBookOrderLifespan = QaToolsOrderLifespan;
 }
 
 parameter_types! {
