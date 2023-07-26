48f2d18c14eb64d4efa0d7804a972f1c531ac695
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-07-26 20:39:15 +0300

    fix buildTag
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -8,12 +8,13 @@ RUSTFLAGS='-Dwarnings'
 RUNTIME_DIR='runtime'
 RUSTC_VERSION=${rustcVersion}
 sudoCheckStatus=${sudoCheckStatus}
+buildTag=${buildTag}
 
 printf "Tag is %s\n" ${TAG_NAME}
-printf "BuildTag is %s\n" ${buildTag}
+printf "BuildTag is %s\n" $buildTag
 # build
 # If TAG_NAME is defined, build for a specific tag
-if [[ -v ${buildTag} || -v ${TAG_NAME} ]]; then
+if [[ -v $buildTag || -v ${TAG_NAME} ]]; then
     if [[ ${TAG_NAME} != '' ]]; then
         if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
             featureList='private-net runtime-benchmarks'
