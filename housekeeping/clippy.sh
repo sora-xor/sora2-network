f29a9d7a0a5d7a46a31a48cf9f52fedc5296996a
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-08 12:35:56 +0300

    test
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/clippy.sh
+++ housekeeping/clippy.sh
@@ -6,12 +6,12 @@ echo $pr
 echo $prBranch
 
 
-if [ "$pr" == true ] && [ "$prBranch" != "master" ]; then
-    printf "👷‍♂️ Starting clippy \n"
+if [[ "$pr" == true && "$prBranch" != "master" ]]; then
+    printf "starting clippy \n"
     SKIP_WASM_BUILD=1 cargo clippy
     SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,runtime-benchmarks
     SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,wip,runtime-benchmarks
 else
-    printf "👷‍♂️ Starting regular clippy \n"
+    printf "starting regular clippy \n"
     cargo clippy -- -D warnings || exit 0
 fi
