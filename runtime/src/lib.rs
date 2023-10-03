e10576a1bc7a8f12dd4d21d796212b26df00843d
bragov4ik <bragov4ik@hotmail.com>
2023-10-03 13:41:06 +0300

    fix issue numbers

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1923,8 +1923,8 @@ impl order_book::Config for Runtime {
     // this limit is mostly because of requirement to use bounded vectors.
     // a user can create multiple accounts at any time.
     type MaxOpenedLimitOrdersPerUser = ConstU32<1024>;
-    type MaxLimitOrdersForPrice = ConstU32<64>; // TODO #711 #723: try to increase the value
-    type MaxSidePriceCount = ConstU32<64>; // TODO #711 #723: try to increase the value
+    type MaxLimitOrdersForPrice = ConstU32<64>; // TODO #711 #748: try to increase the value
+    type MaxSidePriceCount = ConstU32<64>; // TODO #711 #748: try to increase the value
     type MaxExpiringOrdersPerBlock = ConstU32<512>; // TODO #722: clarify parameter
     type MaxExpirationWeightPerBlock = ExpirationsSchedulerMaxWeight;
     type EnsureTradingPairExists = TradingPair;
