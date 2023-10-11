9f8936395d9f8dce313a66946433c52b053510b2
sorabot <admin@soramitsu.co.jp>
2023-10-11 19:11:16 +0000

    Publishing develop documentation
    Signed-off-by: sorabot <admin@soramitsu.co.jp>

--- docs/develop/doc/substrate_gen/runtime/liquidity_proxy/events/sidebar-items.js
+++ docs/develop/doc/substrate_gen/runtime/liquidity_proxy/events/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["BatchSwapExecuted","Exchange","LiquiditySourceDisabled","LiquiditySourceEnabled"]};
+window.SIDEBAR_ITEMS = {"struct":["BatchSwapExecuted","Exchange","LiquiditySourceDisabled","LiquiditySourceEnabled","XorlessTransfer"]};
