0ac52d8d64ceb0d9de064187b69dafccf39638ab
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2023-10-13 14:55:43 +0300

    ci: add gh slither action

--- .github/scripts/comment.js
+++ .github/scripts/comment.js
@@ -2,10 +2,12 @@
 module.exports = async ({ github, context, header, body }) => {
     const comment = [header, body].join("\n");
 
+    const pr_number = await get_pr_number(github, context);
+
     const { data: comments } = await github.rest.issues.listComments({
         owner: context.repo.owner,
         repo: context.repo.repo,
-        issue_number: context.payload.number,
+        issue_number: pr_number,
     });
 
     const botComment = comments.find(
@@ -22,6 +24,23 @@ module.exports = async ({ github, context, header, body }) => {
         body: comment,
         ...(botComment
             ? { comment_id: botComment.id }
-            : { issue_number: context.payload.number }),
+            : { issue_number: pr_number }),
     });
 };
+
+// Returns gh PR number
+async function get_pr_number(github, context) {
+    if (context.issue.number) {
+        // Return issue number if present
+        return context.issue.number;
+    } else {
+        // Otherwise return issue number from commit
+        return (
+            await github.rest.repos.listPullRequestsAssociatedWithCommit({
+                commit_sha: context.sha,
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+            })
+        ).data[0].number;
+    }
+}
