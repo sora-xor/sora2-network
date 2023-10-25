bd759fc5d851c7435f58ca49bc90987739a019d3
Jovan Milošević <ujazaconje@gmail.com>
2023-10-25 14:49:47 +0200

    Upgrade ceres-governance platform

--- pallets/ceres-governance-platform/src/lib.rs
+++ pallets/ceres-governance-platform/src/lib.rs
@@ -12,29 +12,45 @@ mod mock;
 mod tests;
 
 use codec::{Decode, Encode};
-use common::Balance;
+use common::{Balance, BoundedString};
+use frame_support::BoundedVec;
 pub use weights::WeightInfo;
 
 #[derive(Encode, Decode, Default, PartialEq, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Debug))]
-pub struct VotingInfo {
+#[scale_info(skip_type_params(StringLimit))]
+pub struct VotingInfo<StringLimit: sp_core::Get<u32>> {
     /// Voting option
-    voting_option: u32,
+    voting_option: BoundedString<StringLimit>,
     /// Number of votes
     number_of_votes: Balance,
     /// Ceres withdrawn
-    ceres_withdrawn: bool,
+    asset_withdrawn: bool,
 }
 
 #[derive(Encode, Decode, Default, PartialEq, Eq, scale_info::TypeInfo)]
 #[cfg_attr(feature = "std", derive(Debug))]
-pub struct PollInfo<Moment> {
-    /// Number of options
-    pub number_of_options: u32,
+#[scale_info(skip_type_params(StringLimit, OptionsLimit, TitleLimit, DescriptionLimit))]
+pub struct PollInfo<
+    AssetId,
+    Moment,
+    StringLimit: sp_core::Get<u32>,
+    OptionsLimit: sp_core::Get<u32>,
+    TitleLimit: sp_core::Get<u32>,
+    DescriptionLimit: sp_core::Get<u32>,
+> {
+    /// Asset id
+    pub poll_asset: AssetId,
     /// Poll start timestamp
     pub poll_start_timestamp: Moment,
     /// Poll end timestamp
     pub poll_end_timestamp: Moment,
+    /// Poll title
+    pub title: BoundedString<TitleLimit>,
+    /// Description
+    pub description: BoundedString<DescriptionLimit>,
+    /// Options
+    pub options: BoundedVec<BoundedString<StringLimit>, OptionsLimit>,
 }
 
 /// Storage version.
@@ -52,14 +68,20 @@ pub use pallet::*;
 pub mod pallet {
     use crate::{migrations, PollInfo, StorageVersion, VotingInfo, WeightInfo};
     use common::prelude::Balance;
+    use common::BoundedString;
+    use frame_support::pallet_prelude::OptionQuery;
+    use frame_support::pallet_prelude::ValueQuery;
     use frame_support::pallet_prelude::*;
     use frame_support::sp_runtime::traits::AccountIdConversion;
     use frame_support::transactional;
     use frame_support::PalletId;
     use frame_system::ensure_signed;
     use frame_system::pallet_prelude::*;
+    use hex_literal::hex;
     use pallet_timestamp as timestamp;
-    use sp_std::prelude::*;
+    use sp_core::blake2_256;
+    use sp_core::H256;
+    use sp_std::collections::btree_set::BTreeSet;
 
     const PALLET_ID: PalletId = PalletId(*b"ceresgov");
 
@@ -67,11 +89,20 @@ pub mod pallet {
     pub trait Config:
         frame_system::Config + assets::Config + technical::Config + timestamp::Config
     {
+        /// String limit
+        type StringLimit: Get<u32>;
+
+        /// Options limit
+        type OptionsLimit: Get<u32>;
+
+        /// Title limit
+        type TitleLimit: Get<u32>;
+
+        /// Description limit
+        type DescriptionLimit: Get<u32>;
+
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-
-        /// Ceres asset id
-        type CeresAssetId: Get<Self::AssetId>;
 
         /// Weight information for extrinsics in this pallet.
         type WeightInfo: WeightInfo;
@@ -80,6 +111,7 @@ pub mod pallet {
     type Assets<T> = assets::Pallet<T>;
     pub type Timestamp<T> = timestamp::Pallet<T>;
     pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+    type AssetIdOf<T> = <T as assets::Config>::AssetId;
 
     #[pallet::pallet]
     #[pallet::generate_store(pub (super) trait Store)]
@@ -89,13 +121,32 @@ pub mod pallet {
     /// A vote of a particular user for a particular poll
     #[pallet::storage]
     #[pallet::getter(fn votings)]
-    pub type Voting<T: Config> =
-        StorageDoubleMap<_, Identity, Vec<u8>, Identity, AccountIdOf<T>, VotingInfo, ValueQuery>;
+    pub type Voting<T: Config> = StorageDoubleMap<
+        _,
+        Identity,
+        H256,
+        Identity,
+        AccountIdOf<T>,
+        VotingInfo<T::StringLimit>,
+        OptionQuery,
+    >;
 
     #[pallet::storage]
     #[pallet::getter(fn poll_data)]
-    pub type PollData<T: Config> =
-        StorageMap<_, Identity, Vec<u8>, PollInfo<T::Moment>, ValueQuery>;
+    pub type PollData<T: Config> = StorageMap<
+        _,
+        Identity,
+        H256,
+        PollInfo<
+            AssetIdOf<T>,
+            T::Moment,
+            T::StringLimit,
+            T::OptionsLimit,
+            T::TitleLimit,
+            T::DescriptionLimit,
+        >,
+        OptionQuery,
+    >;
 
     #[pallet::type_value]
     pub fn DefaultForPalletStorageVersion<T: Config>() -> StorageVersion {
@@ -108,29 +159,43 @@ pub mod pallet {
     pub type PalletStorageVersion<T: Config> =
         StorageValue<_, StorageVersion, ValueQuery, DefaultForPalletStorageVersion<T>>;
 
+    #[pallet::type_value]
+    pub fn DefaultForAuthorityAccount<T: Config>() -> AccountIdOf<T> {
+        let bytes = hex!("96ea3c9c0be7bbc7b0656a1983db5eed75210256891a9609012362e36815b132");
+        AccountIdOf::<T>::decode(&mut &bytes[..]).unwrap()
+    }
+
+    /// Account which has permissions for changing Hermes minimum amount for voting and creating a poll
+    #[pallet::storage]
+    #[pallet::getter(fn authority_account)]
+    pub type AuthorityAccount<T: Config> =
+        StorageValue<_, AccountIdOf<T>, ValueQuery, DefaultForAuthorityAccount<T>>;
+
     #[pallet::event]
     #[pallet::generate_deposit(pub (super) fn deposit_event)]
     pub enum Event<T: Config> {
         /// Voting [who, poll, option, balance]
-        Voted(AccountIdOf<T>, Vec<u8>, u32, Balance),
-        /// Create poll [who, option, start_timestamp, end_timestamp]
-        Created(AccountIdOf<T>, u32, T::Moment, T::Moment),
+        Voted(AccountIdOf<T>, H256, BoundedString<T::StringLimit>, Balance),
+        /// Create poll [who, title, poll_asset, start_timestamp, end_timestamp]
+        Created(
+            AccountIdOf<T>,
+            BoundedString<T::TitleLimit>,
+            AssetIdOf<T>,
+            T::Moment,
+            T::Moment,
+        ),
         /// Withdrawn [who, balance]
         Withdrawn(AccountIdOf<T>, Balance),
     }
 
     #[pallet::error]
     pub enum Error<T> {
-        /// Invalid votes
-        InvalidVotes,
         /// Poll is finished
         PollIsFinished,
         /// Poll is not started
         PollIsNotStarted,
         /// Not enough funds
         NotEnoughFunds,
-        /// Invalid number of option
-        InvalidNumberOfOption,
         /// Vote denied
         VoteDenied,
         /// Invalid start timestamp
@@ -143,8 +208,20 @@ pub mod pallet {
         InvalidNumberOfVotes,
         /// Funds already withdrawn,
         FundsAlreadyWithdrawn,
-        /// Poll id already exists
-        PollIdAlreadyExists,
+        /// Invalid voting options
+        InvalidVotingOptions,
+        /// Too many voting options
+        TooManyVotingOptions,
+        /// Duplicate options
+        DuplicateOptions,
+        /// Poll does not exist
+        PollDoesNotExist,
+        /// Invalid option
+        InvalidOption,
+        /// Not voted
+        NotVoted,
+        /// Unauthorized
+        Unauthorized,
     }
 
     #[pallet::call]
@@ -155,15 +232,15 @@ pub mod pallet {
         #[pallet::weight(<T as Config>::WeightInfo::vote())]
         pub fn vote(
             origin: OriginFor<T>,
-            poll_id: Vec<u8>,
-            voting_option: u32,
+            poll_id: H256,
+            voting_option: BoundedString<T::StringLimit>,
             number_of_votes: Balance,
         ) -> DispatchResultWithPostInfo {
             let user = ensure_signed(origin)?;
 
             ensure!(number_of_votes > 0, Error::<T>::InvalidNumberOfVotes);
 
-            let poll_info = PollData::<T>::get(&poll_id);
+            let poll_info = <PollData<T>>::get(&poll_id).ok_or(Error::<T>::PollDoesNotExist)?;
             let current_timestamp = Timestamp::<T>::get();
 
             ensure!(
@@ -177,26 +254,30 @@ pub mod pallet {
             );
 
             ensure!(
-                voting_option <= poll_info.number_of_options && voting_option != 0,
-                Error::<T>::InvalidNumberOfOption
+                poll_info.options.contains(&voting_option),
+                Error::<T>::InvalidOption
             );
 
-            let mut voting_info = <Voting<T>>::get(&poll_id, &user);
+            let mut voting_info = VotingInfo {
+                voting_option,
+                number_of_votes: 0,
+                asset_withdrawn: false,
+            };
 
-            if voting_info.voting_option == 0 {
-                voting_info.voting_option = voting_option;
-            } else {
+            // If already voted for one option, then can't vote for another option. But he can increase the number of votes on first option
+            if let Some(mut voting_info) = <Voting<T>>::get(&poll_id, &user) {
                 ensure!(
                     voting_info.voting_option == voting_option,
                     Error::<T>::VoteDenied
-                )
+                );
+                voting_info.number_of_votes += number_of_votes;
+            } else {
+                voting_info.number_of_votes = number_of_votes;
             }
-
-            voting_info.number_of_votes += number_of_votes;
 
             // Transfer Ceres to pallet
             Assets::<T>::transfer_from(
-                &T::CeresAssetId::get(),
+                &poll_info.poll_asset,
                 &user,
                 &Self::account_id(),
                 number_of_votes,
@@ -223,22 +304,19 @@ pub mod pallet {
         #[pallet::weight(<T as Config>::WeightInfo::create_poll())]
         pub fn create_poll(
             origin: OriginFor<T>,
-            poll_id: Vec<u8>,
-            number_of_options: u32,
+            poll_asset: AssetIdOf<T>,
             poll_start_timestamp: T::Moment,
             poll_end_timestamp: T::Moment,
+            title: BoundedString<T::TitleLimit>,
+            description: BoundedString<T::DescriptionLimit>,
+            options: BoundedVec<BoundedString<T::StringLimit>, T::OptionsLimit>,
         ) -> DispatchResultWithPostInfo {
             let user = ensure_signed(origin)?;
-
             let current_timestamp = Timestamp::<T>::get();
 
-            let poll_info = <PollData<T>>::get(&poll_id);
-            ensure!(
-                poll_info.number_of_options == 0,
-                Error::<T>::PollIdAlreadyExists
-            );
-
-            ensure!(number_of_options >= 2, Error::<T>::InvalidNumberOfOption);
+            if user != AuthorityAccount::<T>::get() {
+                return Err(Error::<T>::Unauthorized.into());
+            }
 
             ensure!(
                 poll_start_timestamp >= current_timestamp,
@@ -250,10 +328,30 @@ pub mod pallet {
                 Error::<T>::InvalidEndTimestamp
             );
 
+            let nonce = frame_system::Pallet::<T>::account_nonce(&user);
+            let encoded: [u8; 32] = (&user, nonce).using_encoded(blake2_256);
+            let poll_id = H256::from(encoded);
+
+            let options_len = options.len();
+            if options_len < 2 {
+                return Err(Error::<T>::InvalidVotingOptions.into());
+            }
+            if options_len > 5 {
+                return Err(Error::<T>::TooManyVotingOptions.into());
+            }
+
+            let options_set = BTreeSet::from_iter(&options);
+            if options_set.len() != options_len {
+                return Err(Error::<T>::DuplicateOptions.into());
+            }
+
             let poll_info = PollInfo {
-                number_of_options,
-                poll_end_timestamp,
+                poll_asset,
                 poll_start_timestamp,
+                poll_end_timestamp,
+                title,
+                description,
+                options,
             };
 
             <PollData<T>>::insert(&poll_id, poll_info);
@@ -261,7 +359,8 @@ pub mod pallet {
             //Emit event
             Self::deposit_event(Event::<T>::Created(
                 user,
-                number_of_options,
+                title,
+                poll_asset,
                 poll_start_timestamp,
                 poll_end_timestamp,
             ));
@@ -273,10 +372,10 @@ pub mod pallet {
         /// Withdraw voting funds
         #[pallet::call_index(2)]
         #[pallet::weight(<T as Config>::WeightInfo::withdraw())]
-        pub fn withdraw(origin: OriginFor<T>, poll_id: Vec<u8>) -> DispatchResultWithPostInfo {
+        pub fn withdraw(origin: OriginFor<T>, poll_id: H256) -> DispatchResultWithPostInfo {
             let user = ensure_signed(origin)?;
 
-            let poll_info = PollData::<T>::get(&poll_id);
+            let poll_info = <PollData<T>>::get(&poll_id).ok_or(Error::<T>::PollDoesNotExist)?;
             let current_timestamp = Timestamp::<T>::get();
 
             ensure!(
@@ -285,21 +384,21 @@ pub mod pallet {
             );
 
             // Update storage
-            let mut voting_info = <Voting<T>>::get(&poll_id, &user);
+            let mut voting_info = <Voting<T>>::get(&poll_id, &user).ok_or(Error::<T>::NotVoted)?;
             ensure!(
-                !voting_info.ceres_withdrawn,
+                !voting_info.asset_withdrawn,
                 Error::<T>::FundsAlreadyWithdrawn
             );
 
             // Withdraw CERES
             Assets::<T>::transfer_from(
-                &T::CeresAssetId::get(),
+                &poll_info.poll_asset,
                 &Self::account_id(),
                 &user,
                 voting_info.number_of_votes,
             )?;
 
-            voting_info.ceres_withdrawn = true;
+            voting_info.asset_withdrawn = true;
             <Voting<T>>::insert(&poll_id, &user, &voting_info);
 
             //Emit event
