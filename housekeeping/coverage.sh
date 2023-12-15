87090b6b9ce856d3837ec0fd3c0cd6b3b476cc81
f33r0 <95526886+f33r0@users.noreply.github.com>
2023-12-15 19:38:17 +0300

    Update coverage.sh

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -4,7 +4,6 @@ export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
-rustup component add llvm-tools-preview
 cargo test --features private-net
 
 grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
