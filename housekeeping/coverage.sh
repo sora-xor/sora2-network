45685d77d660b91aaae341e8721d9c79443989be
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-11 19:46:05 +0300

    debug3
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-
+pofrw=$(find . -name 'sora2-*.profraw')
 export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
-export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
-
-echo 'component list:'
-rustup component list
-
-echo 'toolchain list:'
-rustup toolchain list
-
+export LLVM_PROFILE_FILE="$pofrw"
+whereis llvm-profdata
 cargo test --features private-net,ready-to-test,wip
 
 grcov . --binary-path ./target/debug -s . -t html --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
