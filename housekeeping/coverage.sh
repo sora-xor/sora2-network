16343755ca16fcf003e8276d4c7bba3c5f086d59
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-11 18:44:38 +0300

    debug2
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -4,7 +4,11 @@ export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
+echo 'component list:'
 rustup component list
+
+echo 'toolchain list:'
+rustup toolchain list
 
 cargo test --features private-net,ready-to-test,wip
 
