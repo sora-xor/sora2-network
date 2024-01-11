2e684ffde47aaffa2303917a06fedc97159547e7
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-11 19:51:47 +0300

    fix debug3
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-pofrw=$(find . -name 'sora2-*.profraw')
+
 export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
-export LLVM_PROFILE_FILE="$pofrw"
+
 whereis llvm-profdata
+
 cargo test --features private-net,ready-to-test,wip
+
+profraw=$(find . -name 'sora2-*.profraw')
+export LLVM_PROFILE_FILE="$profraw"
 
 grcov . --binary-path ./target/debug -s . -t html --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
 
