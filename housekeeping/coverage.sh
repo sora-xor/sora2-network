c94d8f179057c482b0e2f7ed8973d27e9e86eccd
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-12 17:17:25 +0300

    test 10
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -3,17 +3,9 @@
 export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
-profraw=$(find . -type f -name '*.profraw')
 
 cargo test --features private-net,ready-to-test,wip
 
-echo "Profraw files here: $profraw"
+grcov . --binary-path ./target/debug -s . -t html --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --llvm-path /usr/lib/llvm-13/bin
 
-grcov . --binary-path ./target/debug -s . -t html --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
-
-# Check coverage errors
-if [ $? -eq 1 ]; then
-    exit 1
-fi
-
-#find . -type f -name '*.profraw' -delete
+find . -type f -name '*.profraw' -delete
