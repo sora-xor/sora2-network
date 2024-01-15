ea172b6d1032707c5fcaf6cb0b0ee11244d132d1
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-15 20:11:26 +0300

    test grcov
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -6,6 +6,6 @@ export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
 cargo test --features private-net,ready-to-test,wip
 
-grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --llvm-path /usr/lib/llvm-13/bin
+grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --llvm-path /usr/lib/llvm-17/bin
 
 find . -type f -name '*.profraw' -delete
