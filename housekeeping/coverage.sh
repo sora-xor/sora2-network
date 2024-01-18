92e28a18be1fcaf4524bb60cece20dba36577caf
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-18 16:02:40 +0300

    debug
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -8,6 +8,10 @@ echo 'running tests'
 cargo test --features private-net,ready-to-test,wip
 
 echo 'running coverage'
-grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --llvm-path /usr/lib/llvm-14/bin
+grcov . --binary-path ./target/debug -s . -t cobertura --branch -o cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --llvm-path /usr/lib/llvm-14/bin
+ls -la
+
+
+find . -type f -name 'cobertura_report'
 
 find . -type f -name '*.profraw' -delete
