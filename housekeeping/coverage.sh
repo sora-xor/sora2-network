154ded2b19022ad76328efd9522165a580faef75
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-22 10:56:51 +0300

    test
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -4,8 +4,8 @@ export RUSTFLAGS="-Cinstrument-coverage"
 export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
-echo 'running tests'
-cargo test --features private-net
+# echo 'running tests'
+# cargo test --features private-net
 
 echo 'running coverage'
 grcov . --binary-path ./target/debug -s . -t cobertura --branch -o cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --llvm-path /usr/lib/llvm-14/bin
