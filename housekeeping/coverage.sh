53bd361d2ccbc4dd498e358f3f2440b1e6ac4269
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-23 14:50:13 +0300

    fix coverage
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -2,5 +2,5 @@
 set -e
 
 printf '⚡️ Running coverage %s\n'
-grcov . --binary-path ./target/debug -s . -t cobertura --branch -o cobertura_report.xml --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --log-level "DEBUG" --llvm-path /usr/lib/llvm-14/bin
+grcov . --binary-path ./target/debug -s . -t cobertura --branch -o cobertura_report.xml --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --log-level "ERROR" --llvm-path /usr/lib/llvm-14/bin
 find . -type f -name '*.profraw' -delete
