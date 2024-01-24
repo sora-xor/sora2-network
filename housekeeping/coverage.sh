c8c6e0c97cca77cfe9898db7069bb87cb08922b1
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-01-24 14:12:39 +0300

    rename report
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -3,5 +3,5 @@ set -e
 
 
 printf '⚡️ Running coverage %s\n'
-grcov . --binary-path ./target/debug -s . -t cobertura --branch -o cobertura_report.xml --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --log-level "ERROR" --llvm-path /usr/lib/llvm-14/bin
+grcov . --binary-path ./target/debug -s . -t cobertura --branch -o cobertura.xml --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" "node/src" --log-level "ERROR" --llvm-path /usr/lib/llvm-14/bin
 find . -type f -name '*.profraw' -delete
