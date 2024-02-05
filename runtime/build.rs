d8904884e27b4633eac79545111e3293b00dd655
bragov4ik <bragov4ik@hotmail.com>
2024-02-05 13:51:51 +0700

    trim string

--- runtime/build.rs
+++ runtime/build.rs
@@ -53,16 +53,16 @@ fn main() {
         pre_commit_hook_path.to_string_lossy()
     );
     // Flexible path to support linked worktrees
-    let enabled_hooks_dir: String =  String::from_utf8(
+    let enabled_hooks_dir: String = String::from_utf8(
         std::process::Command::new("git")
             .args(["rev-parse", "--git-path", "hooks"])
             .output()
             .expect("Could not retrieve hooks directory path")
-            .stdout
-    ).expect("Could not convert hooks path to string");
+            .stdout,
+    )
+    .expect("Could not convert hooks path to string");
     let enabled_hooks_dir = PathBuf::new()
-        .join(enabled_hooks_dir)
-        .to_owned();
+        .join(enabled_hooks_dir[..].trim());
     fs::create_dir_all(&enabled_hooks_dir).expect("Failed to create '.git/hooks' dir");
     fs::copy(&pre_commit_hook_path, enabled_hooks_dir.join("pre-commit"))
         .expect("Failed to copy '.hooks/pre_commit' to '.git/hooks/pre_commit'");
