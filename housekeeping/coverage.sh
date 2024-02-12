9413f53584e2b33baf28fa3d38e2174a3224a054
C4tWithShell <shelkovnikov@soramitsu.co.jp>
2024-02-13 01:08:39 +0300

    Fix coverage

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -5,6 +5,6 @@ export SKIP_WASM_BUILD=1
 export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
 cargo test --features private-net
-
+rustup component add llvm-tools-preview
 grcov . --binary-path ./target/debug -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
 find . -type f -name '*.profraw' -delete
