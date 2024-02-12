187c896db80d187bed87565e97d89e8da21ff2d5
C4tWithShell <shelkovnikov@soramitsu.co.jp>
2024-02-12 22:03:26 +0300

    Add sonar, dojo

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -6,5 +6,5 @@ export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
 
 cargo test --features private-net
 
-grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
+grcov . --binary-path ./target/debug -s . -t lcov --branch -o ./lcov_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
 find . -type f -name '*.profraw' -delete
