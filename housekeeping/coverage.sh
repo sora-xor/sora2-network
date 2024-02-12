74d9c659d4aa49446d5441c5c8fd6f9a9138c4e4
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-12 13:35:34 +0300

    update env image
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -3,6 +3,7 @@ set -e
 
 printf '⚡️ Running coverage %s\n'
 # coverage and generate report
-grcov . --binary-path ./target/debug -s . -t lcov --branch -o coverage --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --log-level "ERROR" --llvm-path /usr/lib/llvm-14/bin
-# delete *.profraw
+grcov . --binary-path ./target/debug -s . -t cobertura --branch -o coverage --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --log-level "ERROR" --llvm-path /usr/lib/llvm-14/bin
+
+# delete generated *.profraw
 find . -type f -name '*.profraw' -delete
