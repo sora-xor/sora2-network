92d2a8f2774447bbf41f9fc84749fa228013f8d8
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-13 17:40:31 +0300

    test coverage
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -3,7 +3,7 @@ set -e
 
 printf '⚡️ Running coverage %s\n'
 # coverage and generate report
-grcov . --binary-path ./target/debug -s . -t cobertura --branch -o coverage --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --log-level="ERROR" --llvm-path='/usr/lib/llvm-14/bin'
+grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src" --log-level="ERROR" --llvm-path='/usr/lib/llvm-14/bin'
 
 # delete generated *.profraw
 find . -type f -name '*.profraw' -delete
