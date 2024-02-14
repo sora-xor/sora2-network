0838cf2a13b768ffd536654df621e5cd664f7809
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-14 15:51:09 +0300

    update coverage and env
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -7,3 +7,5 @@ grcov . --binary-path ./target/debug -s . -t lcov --branch -o ./lcov_report --ig
 
 # delete generated *.profraw
 find . -type f -name '*.profraw' -delete
+
+lcov_cobertura lcov_report --output coverage
