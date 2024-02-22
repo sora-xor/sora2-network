581f14eb68d9fdce1d319ce3dc8baca8b16d0495
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-22 07:49:06 +0300

    test new buiild
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,17 +1,21 @@
-@Library('jenkins-library') _
+@Library('jenkins-library@feature/dops-2942-update_rust_lib') _
 
 def pipeline = new org.rust.AppPipeline(steps: this,
       initSubmodules: true,
-      envImageName: 'docker.soramitsu.co.jp/sora2/env:sub4',
+      envImageName: 'docker.soramitsu.co.jp/sora2/env:env',
       appImageName: 'docker.soramitsu.co.jp/sora2/substrate',
       codeCoverageCommand: './housekeeping/coverage.sh',
       cargoDoc: true,
       smartContractScanner: false,
+      clippyLinter: false,
       cargoClippyTag: ':substrate',
       cargoClippyCmds: ['housekeeping/clippy.sh'],
       buildTestCmds: ['housekeeping/build.sh'],
       pushTags: ['add_generic_substrate_app': 'sora-liberland'],
       buildArtifacts: 'framenode_runtime.compact.compressed.wasm, subwasm_report.json, pallet_list.txt',
-      pushToPublicRegistry: true
+      pushToPublicRegistry: true,
+      sonarProjectKey: 'sora:sora2-network',
+      sonarProjectName: 'sora2-network',
+      dojoProductType: 'sora'
 )
 pipeline.runPipeline()
--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -7,32 +7,30 @@ wasmReportFile='subwasm_report.json'
 PACKAGE='framenode-runtime'
 RUSTFLAGS='-Dwarnings'
 RUNTIME_DIR='runtime'
+allfeatures='private-net,wip,ready-to-test'
 
-if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
-    printf "Tag is %s\n" $buildTag ${TAG_NAME}
-else
-    printf "⚡️ There is no tag here. "
-fi
-
-# build
-# If TAG_NAME is defined, build for a specific tag
-if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
-    if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
-        featureList='private-net runtime-benchmarks'
-        sudoCheckStatus=0
-    elif [[ ${TAG_NAME} =~ 'stage'* ]]; then
-        featureList='private-net include-real-files ready-to-test'
-        sudoCheckStatus=0
-    elif [[ ${TAG_NAME} =~ 'test'* ]]; then
-        featureList='private-net include-real-files reduced-pswap-reward-periods ready-to-test'
-        sudoCheckStatus=0
-    elif [[ -n ${TAG_NAME} && ${TAG_NAME} != 'predev' ]]; then
-        featureList='include-real-files'
-        sudoCheckStatus=101
-    fi
-    printf "Building with features: %s\n" "$featureList"
-    printf "Checking sudo pallet: %s\n" "$sudoCheckStatus"
+# build func
+releasetest(){
+    printf "⚡️ Testing with features: private-net runtime-benchmarks %s\n"
     cargo test --release --features "private-net runtime-benchmarks"
+}
+
+test(){
+    printf "⚡️ Running Tests for code coverage only %s\n"
+    export RUSTFLAGS="-Cinstrument-coverage"
+    export SKIP_WASM_BUILD=1
+    export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
+    rm -rf ~/.cargo/.package-cache
+    rm Cargo.lock
+    cargo fmt -- --check > /dev/null
+    cargo test --features $allfeatures
+}
+
+build() {
+    featureList=$1
+    sudoCheckStatus=$2
+    printf "⚡️ Building with features: %s\n" "$featureList"
+    printf "⚡️ Checking sudo pallet: %s\n" "$sudoCheckStatus"
     rm -rf target
     cargo build --release --features "$featureList"
     mv ./target/release/framenode .
@@ -42,16 +40,33 @@ if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; th
     subwasm metadata framenode_runtime.compact.compressed.wasm > $palletListFile
     set +e
     subwasm metadata -m Sudo framenode_runtime.compact.compressed.wasm
-    if [[ $(echo $?) -eq $sudoCheckStatus ]]; then echo "✅ sudo check is successful!"; else echo "❌ sudo check is failed!"; exit 1; fi
-else
-    # If TAG_NAME is not defined, run tests and checks
-    if [[ $prBranch == 'master' ]]; then
-        RUST_LOG="debug cargo test --features try-runtime -- run_migrations"
+    if [[ $? -eq $sudoCheckStatus ]]; then 
+        echo "✅ sudo check is successful!"
+    else 
+        echo "❌ sudo check is failed!"
+        exit 1
     fi
-    printf "⚡️ only tests run %s\n"
-    rm -rf ~/.cargo/.package-cache
-    rm Cargo.lock
-    cargo fmt -- --check > /dev/null
-    cargo test
-    cargo test --features "private-net wip ready-to-test runtime-benchmarks"
+}
+
+# build
+# If TAG_NAME is defined, build for a specific tag
+if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
+    printf "⚡️ Tag is %s\n" $buildTag ${TAG_NAME}
+        releasetest
+    if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
+        build 'private-net runtime-benchmarks' 0
+    elif [[ ${TAG_NAME} =~ 'stage'* ]]; then
+        build 'private-net include-real-files ready-to-test' 0
+    elif [[ ${TAG_NAME} =~ 'test'* ]]; then
+        build 'private-net include-real-files reduced-pswap-reward-periods ready-to-test' 0
+    elif [[ -n ${TAG_NAME} && ${TAG_NAME} != 'predev' ]]; then
+        build 'include-real-files' 101
+    fi
+else
+    if [[ $prBranch == 'master' ]]; then
+        printf "⚡️ Running tests and migrations %s\n"
+        RUST_LOG="debug cargo test --features try-runtime -- run_migrations"
+    else
+        test
+    fi
 fi
