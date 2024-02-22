a3d250257d998eaa0db375c2f836380d567f7560
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-22 12:11:23 +0300

    update build workflow
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -9,24 +9,6 @@ RUSTFLAGS='-Dwarnings'
 RUNTIME_DIR='runtime'
 allfeatures='private-net,wip,ready-to-test'
 
-test
-if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
-    if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
-        build 'private-net runtime-benchmarks' 0
-    elif [[ ${TAG_NAME} =~ 'stage'* ]]; then
-        build 'private-net include-real-files ready-to-test' 0
-    elif [[ ${TAG_NAME} =~ 'test'* ]]; then
-        build 'private-net include-real-files reduced-pswap-reward-periods ready-to-test' 0
-    elif [[ -n ${TAG_NAME} && ${TAG_NAME} != 'predev' ]]; then
-        build 'include-real-files' 101
-    fi
-else
-    if [ $prBranch = 'master' ]; then
-        printf "⚡️ Running tests and migrations %s\n"
-        RUST_LOG="debug cargo test --features try-runtime -- run_migrations"
-    fi
-fi
-
 # build func
 test() {
     if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
@@ -64,3 +46,22 @@ build() {
         exit 1
     fi
 }
+
+
+test
+if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
+    if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
+        build 'private-net runtime-benchmarks' 0
+    elif [[ ${TAG_NAME} =~ 'stage'* ]]; then
+        build 'private-net include-real-files ready-to-test' 0
+    elif [[ ${TAG_NAME} =~ 'test'* ]]; then
+        build 'private-net include-real-files reduced-pswap-reward-periods ready-to-test' 0
+    elif [[ -n ${TAG_NAME} && ${TAG_NAME} != 'predev' ]]; then
+        build 'include-real-files' 101
+    fi
+else
+    if [ $prBranch = 'master' ]; then
+        printf "⚡️ Running tests and migrations %s\n"
+        RUST_LOG="debug cargo test --features try-runtime -- run_migrations"
+    fi
+fi
