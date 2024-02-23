cbfccb9a30cc86b805089b6d941dc5a92f6156a7
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-23 09:53:04 +0300

    update build
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 set -e
 
 # environment
@@ -10,31 +10,47 @@ RUNTIME_DIR='runtime'
 allfeatures='private-net,wip,ready-to-test'
 
 # build func
-releasetest(){
-    printf "⚡️ Testing with features: private-net runtime-benchmarks %s\n"
-    cargo test --release --features "private-net runtime-benchmarks"
-}
-
-test(){
-    printf "⚡️ Running Tests for code coverage only %s\n"
-    export RUSTFLAGS="-Cinstrument-coverage"
-    export SKIP_WASM_BUILD=1
-    export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
-    rm -rf ~/.cargo/.package-cache
-    rm Cargo.lock
-    cargo fmt -- --check > /dev/null
-    cargo test --features $allfeatures
+test() {
+    if [[ ${buildTag} != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
+        printf "Tag is %s\n" ${TAG_NAME} ${buildTag}
+        printf "BuildTag is %s\n" ${buildTag}
+        printf "⚡️ Testing with features: private-net runtime-benchmarks\n"
+        cargo test --release --features "private-net runtime-benchmarks"
+    elif [[ ${prBranch} = 'master' ]]; then
+        printf "⚡️ This is $prbranch Running tests and migrations %s\n"
+        RUST_LOG="debug cargo test --features try-runtime -- run_migrations"
+    else
+        printf "⚡️ Running Tests for code coverage only\n"
+        export RUSTFLAGS="-Cinstrument-coverage"
+        export SKIP_WASM_BUILD=1
+        export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
+        rm -rf ~/.cargo/.package-cache
+        cargo fmt -- --check > /dev/null
+        cargo test --features $allfeatures
+    fi
 }
 
 build() {
-    featureList=$1
-    sudoCheckStatus=$2
+    printf "Tag is %s\n" ${TAG_NAME}
+    printf "BuildTag is %s\n" ${buildTag}
+    sudoCheckStatus="0"
+    if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
+        featureList='private-net runtime-benchmarks'
+    elif [[ ${TAG_NAME} =~ 'stage'* ]]; then
+        featureList='private-net include-real-files ready-to-test'
+    elif [[ ${TAG_NAME} =~ 'test'* || -n ${buildTag} ]]; then
+        featureList='private-net include-real-files reduced-pswap-reward-periods ready-to-test'
+    elif [[ -n ${TAG_NAME} && ${TAG_NAME} != 'predev' ]]; then
+        featureList='include-real-files'
+        sudoCheckStatus="101"
+    elif [[ -n ${buildTag} ]]; then
+        featureList='private-net include-real-files reduced-pswap-reward-periods wip ready-to-test'
+    fi
     printf "⚡️ Building with features: %s\n" "$featureList"
     printf "⚡️ Checking sudo pallet: %s\n" "$sudoCheckStatus"
     rm -rf target
     cargo build --release --features "$featureList"
     mv ./target/release/framenode .
-    mv ./target/release/relayer ./relayer.bin
     mv ./target/release/wbuild/framenode-runtime/framenode_runtime.compact.compressed.wasm ./framenode_runtime.compact.compressed.wasm
     subwasm --json info framenode_runtime.compact.compressed.wasm > $wasmReportFile
     subwasm metadata framenode_runtime.compact.compressed.wasm > $palletListFile
@@ -48,25 +64,8 @@ build() {
     fi
 }
 
-# build
-# If TAG_NAME is defined, build for a specific tag
-if [[ $buildTag != null ]] && [[ ${TAG_NAME} != null || ${TAG_NAME} != '' ]]; then
-    printf "⚡️ Tag is %s\n" $buildTag ${TAG_NAME}
-        releasetest
-    if [[ ${TAG_NAME} =~ 'benchmarking'* ]]; then
-        build 'private-net runtime-benchmarks' 0
-    elif [[ ${TAG_NAME} =~ 'stage'* ]]; then
-        build 'private-net include-real-files ready-to-test' 0
-    elif [[ ${TAG_NAME} =~ 'test'* ]]; then
-        build 'private-net include-real-files reduced-pswap-reward-periods ready-to-test' 0
-    elif [[ -n ${TAG_NAME} && ${TAG_NAME} != 'predev' ]]; then
-        build 'include-real-files' 101
-    fi
+if [ "$(type -t $1)" = "function" ]; then
+    "$1"
 else
-    if [[ $prBranch == 'master' ]]; then
-        printf "⚡️ Running tests and migrations %s\n"
-        RUST_LOG="debug cargo test --features try-runtime -- run_migrations"
-    else
-        test
-    fi
+    echo "Func '$1' is not exists in this workflow. Skipped."
 fi
