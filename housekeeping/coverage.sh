4f05ab7a28dda0ea2826933d66dfd028bd5c731a
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-27 20:40:46 +0300

    fix
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/coverage.sh
+++ housekeeping/coverage.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-export RUSTFLAGS="-Cinstrument-coverage"
-export SKIP_WASM_BUILD=1
-export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
-
-cargo test --features private-net
-
 grcov . --binary-path ./target/debug -s . -t cobertura --branch -o ./cobertura_report --ignore-not-existing --ignore  "/opt/cargo/**" "target/debug" "node/src"
 find . -type f -name '*.profraw' -delete
