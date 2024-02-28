84f74926a03791cf58ce71aebec04ba1ac3da8d5
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2024-02-28 12:46:32 +0300

    coverage optimization
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -24,7 +24,7 @@ test() {
         export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
         rm -rf ~/.cargo/.package-cache
         cargo fmt -- --check > /dev/null
-        cargo test --features $allfeatures
+        cargo test --features $allfeatures -- --test-threads=2
     fi
 }
 
