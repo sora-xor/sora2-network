44a500da2ce2d794377eda965562fe47169d2c4b
Danijel Radulović <master.daca09@gmail.com>
2024-03-22 13:15:41 +0100

    Implement unimplemented trait item

--- pallets/kensetsu/src/mock.rs
+++ pallets/kensetsu/src/mock.rs
@@ -81,6 +81,10 @@ impl PriceToolsProvider<AssetId> for PriceToolsMock {
     fn register_asset(_asset_id: &AssetId) -> DispatchResult {
         unimplemented!()
     }
+
+    fn spot_price(_asset_id: &AssetId) -> Result<Balance, sp_runtime::DispatchError> {
+        unimplemented!()
+    }
 }
 
 pub struct MockLiquidityProxy;
