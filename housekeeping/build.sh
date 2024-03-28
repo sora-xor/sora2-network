18acce7f99a6611f1bd9bdd6b2f8323ef4c21b90
f33r0 <95526886+f33r0@users.noreply.github.com>
2024-03-28 14:51:52 +0300

    Update build.sh

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -21,7 +21,7 @@ test() {
         export LLVM_PROFILE_FILE="sora2-%p-%m.profraw"
         rm -rf ~/.cargo/.package-cache
         cargo fmt -- --check > /dev/null
-        cargo test --features $allfeatures -- --test-threads=2 
+        cargo test --features "$allfeatures" -- --test-threads=2 
     fi
     if [[ $prBranch = 'master' ]]; then
         printf "⚡️ This is "$prbranch" Running tests and migrations %s\n"
