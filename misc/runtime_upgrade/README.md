566f6970aea249895237764cb477f7b27f781f8e
Haidar Jbeily <h.jbeily@innopolis.university>
2024-04-11 20:29:24 +0300

    add automatical runtime upgrade python script

---
+++ misc/runtime_upgrade/README.md
@@ -0,0 +1,55 @@
+# Runtime Upgrade Script
+
+Upgrade Runtime of a Substrate node
+
+## Requirements
+
+- Python 3.11+
+
+  Or
+
+- Docker
+
+## How to use
+
+### Manual Setup
+
+1. Install packages
+
+```bash
+pip install -r requirements.txt
+```
+
+2. Run script with the specific arguments
+
+```bash
+python main.py --node-url ws://0.0.0.0:9944 --uri //Alice
+```
+
+### Using Docker
+
+#### Build Docker Image
+
+```bash
+docker build -t runtime-upgrade .
+```
+
+#### Run Docker Image
+
+```bash
+docker run --rm runtime-upgrade --node-url ws://0.0.0.0:9944 --uri //Alice
+```
+
+### Arguments
+
+```
+usage: Runtime Upgrade [-h] [--node-url NODE_URL]
+(--uri URI_KEYPAIR | --seed SEED | --mnemonic MNEMONIC)
+
+options:
+-h, --help show this help message and exit
+--node-url NODE_URL URL of the node to connect to
+--uri URI_KEYPAIR URI of the keypair to use
+--seed SEED Seed of the keypair to use
+--mnemonic MNEMONIC Seed phrase of the keypair to use
+```
---
+++ misc/runtime_upgrade/main.py
@@ -0,0 +1,91 @@
+from substrateinterface import SubstrateInterface, Keypair
+from substrateinterface.exceptions import SubstrateRequestException
+from scalecodec.types import GenericCall
+import argparse
+
+ss58_format = 69
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='Runtime Upgrade', description='Upgrade Runtime of a Substrate node')
+    parser.add_argument('--node-url', help='URL of the node to connect to', dest='node_url', default='ws://127.0.0.1:9944', required=False)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--uri', help='URI of the keypair to use', dest='uri_keypair', type=str)
+    group.add_argument('--seed', help='Seed of the keypair to use', dest='seed', type=str)
+    group.add_argument('--mnemonic', help='Seed phrase of the keypair to use', dest='mnemonic', type=str)
+    args = parser.parse_args()
+    return args
+
+def get_keypair_using_args(args):
+    if args.uri_keypair:
+        return Keypair.create_from_uri(args.uri_keypair, ss58_format=ss58_format)
+    elif args.seed:
+        return Keypair.create_from_seed(args.seed, ss58_format=ss58_format)
+    elif args.mnemonic:
+        return Keypair.create_from_mnemonic(args.mnemonic, ss58_format=ss58_format)
+    else:
+        raise Exception("No keypair provided")
+    
+
+def send_extrinsic(substrate_provider: SubstrateInterface, keypair: Keypair, call: GenericCall):
+    extrinsic = substrate_provider.create_signed_extrinsic(call=call, keypair=keypair)
+    try:
+        receipt = substrate_provider.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+        log_receipt(receipt)
+    except SubstrateRequestException as e:
+        print(f'Error in send_extrinsic: {e}')
+        raise e
+
+def log_receipt(receipt):
+    print('Extrinsic "{}" included in block "{}"'.format(
+        receipt.extrinsic_hash, receipt.block_hash
+    ))
+
+    if receipt.is_success:
+        print('✅ Success, triggered events:')
+        for event in receipt.triggered_events:
+            print(f'* {event.value}')
+    else:
+        print('⚠️ Extrinsic Failed: ', receipt.error_message)
+    
+
+def get_new_code_from_wasm_file(wasm_file_path):
+    with open(wasm_file_path, 'rb') as f:
+        code = '0x' + f.read().hex()
+    return code
+
+def main():
+    substrate = None
+    try:
+        args = parse_args()
+        keypair = get_keypair_using_args(args)
+        substrate = SubstrateInterface(
+            url=args.node_url,
+            ss58_format=ss58_format,
+        )
+          
+        call = substrate.compose_call(
+            call_module='Sudo',
+            call_function='sudo_unchecked_weight',
+            call_params={
+                'call': {
+                    'call_module': 'System',
+                    'call_function':'set_code',
+                    'call_args': {
+                        'code': get_new_code_from_wasm_file('framenode_runtime.compact.compressed.wasm')
+                    }
+                },
+                'weight': {'ref_time': 0, 'proof_size': 0}
+            }
+        )
+        
+        send_extrinsic(substrate, keypair, call)
+    except Exception as e:
+        print("Error: {}".format(e))
+    finally:
+        if substrate:
+            substrate.close()
+        
+        
+
+if __name__ == '__main__':
+    main()
