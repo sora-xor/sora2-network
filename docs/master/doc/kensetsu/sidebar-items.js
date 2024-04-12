1df804169365524718ddc1b564bb319e0cfdfb15
sorabot <admin@soramitsu.co.jp>
2024-04-12 10:47:32 +0000

    Publishing master documentation
    Signed-off-by: sorabot <admin@soramitsu.co.jp>

--- docs/master/doc/kensetsu/sidebar-items.js
+++ docs/master/doc/kensetsu/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["TECH_ACCOUNT_PREFIX","TECH_ACCOUNT_TREASURY_MAIN"],"mod":["pallet","weights"],"struct":["CollateralInfo","CollateralRiskParameters","CollateralizedDebtPosition"]};
+window.SIDEBAR_ITEMS = {"constant":["TECH_ACCOUNT_PREFIX","TECH_ACCOUNT_TREASURY_MAIN"],"enum":["CdpType"],"mod":["pallet","weights"],"struct":["CollateralInfo","CollateralRiskParameters","CollateralizedDebtPosition"]};
