1eb211319eecab8659e3bac39754ab1013a42ccc
Vladimir Stepanenko <vovac12@gmail.com>
2024-04-17 10:47:19 +0300

    Fix clippy

--- runtime/src/migrations.rs
+++ runtime/src/migrations.rs
@@ -28,5 +28,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use crate::*;
+
 pub type Migrations =
     (bridge_proxy::migrations::generic_account_v2::LiberlandGenericAccount<Runtime>,);
