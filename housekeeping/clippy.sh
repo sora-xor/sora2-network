6f3291fd306dbe3363a81bfca620d9cae6c4f442
Vladimir Stepanenko <vovac12@gmail.com>
2024-04-18 22:24:49 +0300

    Fix Kensetsu migration

--- housekeeping/clippy.sh
+++ housekeeping/clippy.sh
@@ -3,6 +3,7 @@ set -e
 
 if [ "$pr" = true ]; then
     printf "👷‍♂️ starting clippy \n"
+    SKIP_WASM_BUILD=1 cargo clippy --features private-net,runtime-benchmarks,try-runtime -- -D warnings
     SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,runtime-benchmarks,try-runtime -- -D warnings
     SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,wip,runtime-benchmarks,try-runtime --message-format=json -- -D warnings > clippy_report.json
 else
