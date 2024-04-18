74f5809ea867593508a494259dd5c5f5e8174fa6
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2024-04-18 19:54:31 +0300

    feature(kensetsu): runtime migration - registers Kensetsu Treasury technical account

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -259,10 +259,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 76,
+    spec_version: 77,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 76,
+    transaction_version: 77,
     state_version: 0,
 };
 
