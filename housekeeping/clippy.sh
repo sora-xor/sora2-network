efd47f342589eb9900fddb01865ff5439a881c79
Vladimir Stepanenko <vovac12@gmail.com>
2024-04-18 12:34:08 +0300

    Fix try-runtime build (#992)

--- housekeeping/clippy.sh
+++ housekeeping/clippy.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$pr" = true ]; then
     printf "👷‍♂️ starting clippy \n"
-    SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,runtime-benchmarks -- -D warnings
-    SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,wip,runtime-benchmarks --message-format=json -- -D warnings > clippy_report.json
+    SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,runtime-benchmarks,try-runtime -- -D warnings
+    SKIP_WASM_BUILD=1 cargo clippy --features private-net,ready-to-test,wip,runtime-benchmarks,try-runtime --message-format=json -- -D warnings > clippy_report.json
 else
     printf "👷‍♂️ starting a regular clippy \n"
     cargo clippy --message-format=json > clippy_report.json || exit 0
