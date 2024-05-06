ef3bdef1f9544bb5ebdb64a8bd74ce951e98af48
Alexey <Alexey.N.Chernyshov@gmail.com>
2024-05-06 11:46:04 +0300

    chore(kensetsu): bump chainspec for stage runtime upgrade (#1028)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -256,10 +256,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 79,
+    spec_version: 80,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 79,
+    transaction_version: 80,
     state_version: 0,
 };
 
