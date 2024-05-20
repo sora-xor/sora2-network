e94ec65b89062185255a13a19ff54822c7ccdfa6
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2024-05-20 12:43:55 +0300

    feature(kensetsu): update migration

--- runtime/src/migrations.rs
+++ runtime/src/migrations.rs
@@ -28,11 +28,6 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#[cfg(feature = "ready-to-test")] // kensetsu
 use crate::*;
 
-#[cfg(not(feature = "ready-to-test"))]
-pub type Migrations = ();
-
-#[cfg(feature = "ready-to-test")] // kensetsu
 pub type Migrations = (kensetsu::migrations::v1_to_v2::UpgradeToV2<Runtime>,);
