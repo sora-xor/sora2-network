8864ca6f3fea82f0c68df4ce2f6ad7294b3ff00b
Dmitry <bushuevdv@mail.ru>
2024-05-25 00:41:23 -0300

    fix all rpc

--- pallets/dex-manager/rpc/src/lib.rs
+++ pallets/dex-manager/rpc/src/lib.rs
@@ -75,11 +75,11 @@ where
 {
     fn list_dex_ids(&self, at: Option<<Block as BlockT>::Hash>) -> Result<Vec<DEXId>> {
         let api = self.client.runtime_api();
-        let at = BlockId::hash(at.unwrap_or(
+        let at = at.unwrap_or(
             // If the block hash is not supplied assume the best block.
             self.client.info().best_hash,
-        ));
-        api.list_dex_ids(&at)
+        );
+        api.list_dex_ids(at)
             .map_err(|e| RpcError::Call(CallError::Failed(e.into())))
     }
 }
