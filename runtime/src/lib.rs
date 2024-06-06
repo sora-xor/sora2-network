8508f59cc0382317c17e10a87ffdf1b134f82790
Vladimir Stepanenko <vovac12@gmail.com>
2024-06-07 00:19:22 +0700

    Increase runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -253,10 +253,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 85,
+    spec_version: 86,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 85,
+    transaction_version: 86,
     state_version: 0,
 };
 
