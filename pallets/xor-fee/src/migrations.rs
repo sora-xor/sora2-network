6a21b211f695dd3990e68993f74feb9b246fa4e7
Taras Rusakovich <tarus01@mail.ru>
2024-06-12 17:28:56 +0300

    add migration

---
+++ pallets/xor-fee/src/migrations.rs
@@ -0,0 +1,79 @@
+// This file is part of the SORA network and Polkaswap app.
+
+// Copyright (c) 2020, 2021, Polka Biome Ltd. All rights reserved.
+// SPDX-License-Identifier: BSD-4-Clause
+
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+
+// Redistributions of source code must retain the above copyright notice, this list
+// of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this
+// list of conditions and the following disclaimer in the documentation and/or other
+// materials provided with the distribution.
+//
+// All advertising materials mentioning features or use of this software must display
+// the following acknowledgement: This product includes software developed by Polka Biome
+// Ltd., SORA, and Polkaswap.
+//
+// Neither the name of the Polka Biome Ltd. nor the names of its contributors may be used
+// to endorse or promote products derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY Polka Biome Ltd. AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Polka Biome Ltd. BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#[cfg(feature = "ready-to-test")] // Dynamic fee
+pub mod v2 {
+    use frame_support::dispatch::Weight;
+    use frame_support::traits::OnRuntimeUpgrade;
+    use frame_support::{log::info, traits::StorageVersion};
+
+    use crate::*;
+
+    pub struct Migrate<T>;
+
+    impl<T> OnRuntimeUpgrade for Migrate<T>
+    where
+        T: Config,
+    {
+        fn on_runtime_upgrade() -> Weight {
+            if StorageVersion::get::<Pallet<T>>() < StorageVersion::new(2) {
+                // 1 read
+                let next_update_block = <frame_system::Pallet<T>>::block_number();
+                NextUpdateBlock::<T>::put(Some(next_update_block)); // 1 write
+                info!("NextUpdateBlock initialized to {:?}", next_update_block);
+                StorageVersion::new(2).put::<Pallet<T>>();
+                return T::DbWeight::get().reads_writes(1, 1);
+            }
+            Weight::default()
+        }
+
+        #[cfg(feature = "try-runtime")]
+        fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
+            frame_support::ensure!(
+                StorageVersion::get::<Pallet<T>>() < StorageVersion::new(2),
+                "Wrong storage version before upgrade"
+            );
+            Ok(Vec::new())
+        }
+
+        #[cfg(feature = "try-runtime")]
+        fn post_upgrade(_state: Vec<u8>) -> Result<(), &'static str> {
+            frame_support::ensure!(
+                StorageVersion::get::<Pallet<T>>() == StorageVersion::new(2),
+                "Wrong storage version after upgrade"
+            );
+            frame_support::ensure!(
+                NextUpdateBlock::<T>::get() == Some(<frame_system::Pallet<T>>::block_number()),
+                "Did not set right next update block"
+            );
+            Ok(())
+        }
+    }
+}
