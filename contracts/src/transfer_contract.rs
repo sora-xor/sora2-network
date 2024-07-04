27ac6819bd2e6584406c5ceca3586baca66c3e3f
Taras Rusakovich <tarus01@mail.ru>
2024-07-04 12:52:56 +0300

    add doc-strings to ink contracts example of transfer call

--- contracts/src/transfer_contract.rs
+++ contracts/src/transfer_contract.rs
@@ -3,13 +3,18 @@ use common::{AssetId32, Balance, PredefinedAssetId};
 use ink::primitives::AccountId;
 use scale::Encode;
 
-// TODO: Add comments with description
+/// It is a part of the runtime dispatchables API.
+/// `Ink!` doesn't expose the real enum, so we need a partial definition matching our targets.
+/// You should get or count index of the pallet, using `construct_runtime!`, it is zero based
 #[derive(Encode)]
 pub enum RuntimeCall {
     #[codec(index = 21)]
     Assets(AssetsCall),
 }
 
+/// It is a part of a pallet dispatchables API.
+/// The indexes can be found in your pallet code's #[pallet::call] section and check #[pallet::call_index(x)] attribute of the call.
+/// If these attributes are missing, use source-code order (0-based).
 #[derive(Encode)]
 pub enum AssetsCall {
     #[codec(index = 1)]
@@ -42,6 +47,8 @@ mod asset_contract {
             Default::default()
         }
 
+        /// Transfer amount of asset from caller to another account.
+        /// You may found list of callable extrinsic in `pallet_contracts::Config::CallFilter`
         #[ink(message)]
         pub fn transfer(
             &self,
