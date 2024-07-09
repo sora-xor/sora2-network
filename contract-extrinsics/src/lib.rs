9c26f62f7893d7d916c5d9db8ed52bca6bd99ac4
Taras Rusakovich <tarus01@mail.ru>
2024-07-09 13:27:28 +0300

    assets extrinsics with bounds

--- contract-extrinsics/src/lib.rs
+++ contract-extrinsics/src/lib.rs
@@ -1,5 +1,65 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use crate::assets::AssetsCall;
+use crate::order_book::OrderBookCall;
+use common::{AssetId32, PredefinedAssetId};
+use frame_support::pallet_prelude::{MaybeSerializeDeserialize, Member};
+use frame_support::sp_runtime::testing::H256;
+use frame_support::sp_runtime::traits::MaybeDisplay;
+use frame_support::Parameter;
+use scale::{Encode, MaxEncodedLen};
+use sp_std::fmt::Debug;
 pub mod assets;
 
 pub mod order_book;
+
+/// It is a part of the runtime dispatchables API.
+/// `Ink!` doesn't expose the real enum, so we need a partial definition matching our targets.
+/// You should get or count index of the pallet, using `construct_runtime!`, it is zero based
+#[derive(Encode)]
+pub enum RuntimeCall<AssetId: AssetIdBounds, AccountId: AccountIdBounds> {
+    #[codec(index = 21)]
+    Assets(AssetsCall<AssetId, AccountId>),
+    #[codec(index = 57)]
+    OrderBook(OrderBookCall),
+}
+
+pub trait AssetIdBounds:
+    Parameter
+    + Member
+    + Copy
+    + MaybeSerializeDeserialize
+    + Ord
+    + Default
+    + Into<AssetId32<PredefinedAssetId>>
+    + From<AssetId32<PredefinedAssetId>>
+    + From<H256>
+    + Into<H256>
+    + MaxEncodedLen
+{
+}
+
+impl<T> AssetIdBounds for T where
+    T: Parameter
+        + Member
+        + Copy
+        + MaybeSerializeDeserialize
+        + Ord
+        + Default
+        + Into<AssetId32<PredefinedAssetId>>
+        + From<AssetId32<PredefinedAssetId>>
+        + From<H256>
+        + Into<H256>
+        + MaxEncodedLen
+{
+}
+
+pub trait AccountIdBounds:
+    Parameter + Member + MaybeSerializeDeserialize + Debug + MaybeDisplay + Ord + MaxEncodedLen
+{
+}
+
+impl<T> AccountIdBounds for T where
+    T: Parameter + Member + MaybeSerializeDeserialize + Debug + MaybeDisplay + Ord + MaxEncodedLen
+{
+}
--- contracts/src/transfer_contract.rs
+++ contracts/src/transfer_contract.rs
@@ -1,35 +1,12 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-use common::{AssetId32, Balance, PredefinedAssetId};
-use ink::primitives::AccountId;
-use scale::Encode;
-
-/// It is a part of the runtime dispatchables API.
-/// `Ink!` doesn't expose the real enum, so we need a partial definition matching our targets.
-/// You should get or count index of the pallet, using `construct_runtime!`, it is zero based
-#[derive(Encode)]
-pub enum RuntimeCall {
-    #[codec(index = 21)]
-    Assets(AssetsCall),
-}
-
-/// It is a part of a pallet dispatchables API.
-/// The indexes can be found in your pallet code's #[pallet::call] section and check #[pallet::call_index(x)] attribute of the call.
-/// If these attributes are missing, use source-code order (0-based).
-#[derive(Encode)]
-pub enum AssetsCall {
-    #[codec(index = 1)]
-    Transfer {
-        asset_id: AssetId32<PredefinedAssetId>,
-        to: AccountId,
-        amount: Balance,
-    },
-}
 
 #[ink::contract]
 mod asset_contract {
-    use crate::transfer_contract::{AssetsCall, RuntimeCall};
     use common::AssetId32;
+    use contract_extrinsics::assets::AssetsCall;
+    use contract_extrinsics::RuntimeCall;
     use scale::{Decode, Encode};
+    use sp_runtime::AccountId32;
 
     #[ink(storage)]
     #[derive(Default)]
@@ -47,13 +24,11 @@ mod asset_contract {
             Default::default()
         }
 
-        /// Transfer amount of asset from caller to another account.
-        /// You may found list of callable extrinsic in `pallet_contracts::Config::CallFilter`
         #[ink(message)]
         pub fn transfer(
             &self,
             asset_id: [u8; 32],
-            to: AccountId,
+            to: AccountId32,
             amount: Balance,
         ) -> Result<(), RuntimeError> {
             self.env()
