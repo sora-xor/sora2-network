0bd8a041fa0b7a096dd332523c44b4efdeea27cd
Vladimir Stepanenko <vovac12@gmail.com>
2024-07-23 00:21:50 +0700

    Prepare release 3.5.1

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -253,10 +253,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 90,
+    spec_version: 91,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 90,
+    transaction_version: 91,
     state_version: 0,
 };
 
@@ -961,10 +961,13 @@ impl assets::Config for Runtime {
     type Currency = currencies::Pallet<Runtime>;
     type GetTotalBalance = GetTotalBalance;
     type WeightInfo = assets::weights::SubstrateWeight<Runtime>;
+    #[cfg(feature = "ready-to-test")] // DeFi-R
     type AssetRegulator = (
         permissions::Pallet<Runtime>,
         extended_assets::Pallet<Runtime>,
     );
+    #[cfg(not(feature = "ready-to-test"))] // DeFi-R
+    type AssetRegulator = permissions::Pallet<Runtime>;
 }
 
 impl trading_pair::Config for Runtime {
@@ -1044,7 +1047,10 @@ impl pool_xyk::Config for Runtime {
     type GetChameleonPool = GetChameleonPool;
     type GetChameleonPoolBaseAssetId = GetChameleonPoolBaseAssetId;
     type AssetInfoProvider = assets::Pallet<Runtime>;
+    #[cfg(feature = "ready-to-test")] // DeFi-R
     type AssetRegulator = extended_assets::Pallet<Runtime>;
+    #[cfg(not(feature = "ready-to-test"))] // DeFi-R
+    type AssetRegulator = ();
     type IrreducibleReserve = GetXykIrreducibleReservePercent;
     type WeightInfo = pool_xyk::weights::SubstrateWeight<Runtime>;
 }
@@ -2421,6 +2427,7 @@ impl multisig_verifier::Config for Runtime {
     type ThisNetworkId = ThisNetworkId;
 }
 
+#[cfg(feature = "ready-to-test")] // DeFi-R
 impl extended_assets::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type AssetInfoProvider = Assets;
@@ -2552,6 +2559,7 @@ construct_runtime! {
         QaTools: qa_tools::{Pallet, Call, Event<T>} = 112,
 
         ApolloPlatform: apollo_platform::{Pallet, Call, Storage, Event<T>, ValidateUnsigned} = 114,
+        #[cfg(feature = "ready-to-test")] // DeFi-R
         ExtendedAssets: extended_assets::{Pallet, Call, Storage, Event<T>} = 115,
     }
 }
@@ -3315,6 +3323,7 @@ impl_runtime_apis! {
             list_benchmark!(list, extra, substrate_bridge_app, SubstrateBridgeApp);
             list_benchmark!(list, extra, bridge_data_signer, BridgeDataSigner);
             list_benchmark!(list, extra, multisig_verifier, MultisigVerifier);
+            #[cfg(feature = "ready-to-test")] // DeFi-R
             list_benchmark!(list, extra, extended_assets, ExtendedAssets);
 
             let storage_info = AllPalletsWithSystem::storage_info();
@@ -3411,6 +3420,7 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, substrate_bridge_app, SubstrateBridgeApp);
             add_benchmark!(params, batches, bridge_data_signer, BridgeDataSigner);
             add_benchmark!(params, batches, multisig_verifier, MultisigVerifier);
+            #[cfg(feature = "ready-to-test")] // DeFi-R
             add_benchmark!(params, batches, extended_assets, ExtendedAssets);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
