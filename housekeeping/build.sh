5b720e9717cd5c0dfd3aa9162c121b71b3cbdac2
Nikita Zaporozhets <nan3251234@gmail.com>
2024-07-31 17:27:03 +0300

    fix logic
    Signed-off-by: Nikita Zaporozhets <nan3251234@gmail.com>

--- housekeeping/build.sh
+++ housekeeping/build.sh
@@ -15,6 +15,7 @@ test() {
         printf "⚡️ Testing with features: private-net runtime-benchmarks\n"
         cargo test --release --features "private-net runtime-benchmarks" -- --test-threads 2 -- -j 4
         if [[ ${TAG_NAME} =~ 'testnet'* || ($specialLabels == 'true' && $prWasMerged == 'true') ]]; then
+            printf "⚡️ Testing with checks\n"
             RUST_LOG="debug"
             cargo test -j 3 --features try-runtime -- run_migrations
         fi
