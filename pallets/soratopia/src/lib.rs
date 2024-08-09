be55899ba34c77c1e3e6d3fa17d81b9adc8abca3
Alexey <Alexey.N.Chernyshov@gmail.com>
2024-08-09 15:03:26 +0300

    feature(soratopia): adds soratopia pallet (#1158)
    feature(soratopia): update benchmark
    
    feature(soratopia): rm event
    
    feature(soratopia): benchmarking refactoring

---
+++ pallets/soratopia/src/lib.rs
@@ -0,0 +1,102 @@
+// This file is part of the SORA network and Polkaswap app.
+
+// Copyright (c) 2020, 2021, Polka Biome Ltd. All rights reserved.
+// SPDX-License-Identifier: BSD-4-Clause
+
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+
+// Redistributions of source code must retain the above copyright notice, this list
+// of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this
+// list of conditions and the following disclaimer in the documentation and/or other
+// materials provided with the distribution.
+//
+// All advertising materials mentioning features or use of this software must display
+// the following acknowledgement: This product includes software developed by Polka Biome
+// Ltd., SORA, and Polkaswap.
+//
+// Neither the name of the Polka Biome Ltd. nor the names of its contributors may be used
+// to endorse or promote products derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY Polka Biome Ltd. AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Polka Biome Ltd. BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+mod benchmarking;
+
+pub mod weights;
+
+pub use pallet::*;
+
+/// Soratopia game pallet
+#[frame_support::pallet]
+pub mod pallet {
+    use super::*;
+    use crate::weights::WeightInfo;
+    use common::{AssetManager, Balance, XOR};
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    /// The current storage version.
+    const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::storage_version(STORAGE_VERSION)]
+    pub struct Pallet<T>(PhantomData<T>);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config + common::Config {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        #[pallet::constant]
+        type AdminAccount: Get<Self::AccountId>;
+
+        #[pallet::constant]
+        type CheckInTransferAmount: Get<Balance>;
+
+        /// Weight information for extrinsics in this pallet.
+        type WeightInfo: WeightInfo;
+    }
+
+    #[pallet::event]
+    pub enum Event<T: Config> {}
+
+    #[pallet::error]
+    pub enum Error<T> {}
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<T::BlockNumber> for Pallet<T> {}
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Soratopia on-chain check in.
+        /// Transfers XOR from caller to admin account.
+        #[pallet::call_index(0)]
+        #[pallet::weight(< T as Config>::WeightInfo::check_in())]
+        pub fn check_in(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+            let who = ensure_signed(origin)?;
+            T::AssetManager::transfer_from(
+                &XOR.into(),
+                &who,
+                &T::AdminAccount::get(),
+                T::CheckInTransferAmount::get(),
+            )?;
+            Ok(().into())
+        }
+    }
+}
---
+++ pallets/soratopia/src/mock.rs
@@ -0,0 +1,125 @@
+// This file is part of the SORA network and Polkaswap app.
+
+// Copyright (c) 2020, 2021, Polka Biome Ltd. All rights reserved.
+// SPDX-License-Identifier: BSD-4-Clause
+
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+
+// Redistributions of source code must retain the above copyright notice, this list
+// of conditions and the following disclaimer.
+// Redistributions in binary form must reproduce the above copyright notice, this
+// list of conditions and the following disclaimer in the documentation and/or other
+// materials provided with the distribution.
+//
+// All advertising materials mentioning features or use of this software must display
+// the following acknowledgement: This product includes software developed by Polka Biome
+// Ltd., SORA, and Polkaswap.
+//
+// Neither the name of the Polka Biome Ltd. nor the names of its contributors may be used
+// to endorse or promote products derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY Polka Biome Ltd. AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Polka Biome Ltd. BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use crate as soratopia;
+
+use common::mock::ExistentialDeposits;
+use common::Amount;
+use common::{
+    mock_assets_config, mock_common_config, mock_currencies_config, mock_frame_system_config,
+    mock_pallet_balances_config, mock_permissions_config, mock_technical_config,
+    mock_tokens_config, AssetId32, DEXId, PredefinedAssetId, XOR, XST,
+};
+use currencies::BasicCurrencyAdapter;
+use frame_support::parameter_types;
+use frame_support::traits::Everything;
+use frame_system::offchain::SendTransactionTypes;
+use hex_literal::hex;
+use sp_core::crypto::AccountId32;
+use sp_core::H256;
+use sp_runtime::MultiSignature;
+use sp_runtime::{
+    testing::{Header, TestXt},
+    traits::{BlakeTwo256, IdentifyAccount, IdentityLookup, Verify},
+};
+
+type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+type AssetId = AssetId32<PredefinedAssetId>;
+type Balance = u128;
+type Block = frame_system::mocking::MockBlock<TestRuntime>;
+type BlockNumber = u64;
+type Signature = MultiSignature;
+type TechAccountId = common::TechAccountId<AccountId, TechAssetId, DEXId>;
+type TechAssetId = common::TechAssetId<PredefinedAssetId>;
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<TestRuntime>;
+
+frame_support::construct_runtime!(
+    pub enum TestRuntime where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Pallet, Call, Storage, Event<T>},
+        Assets: assets::{Pallet, Call, Storage, Config<T>, Event<T>},
+        Balances: pallet_balances::{Pallet, Call, Storage, Event<T>},
+        Technical: technical::{Pallet, Call, Config<T>, Event<T>},
+        Tokens: tokens::{Pallet, Call, Config<T>, Storage, Event<T>},
+        Permissions: permissions::{Pallet, Call, Config<T>, Storage, Event<T>},
+        Soratopia: soratopia::{Pallet, Call, Storage, Event<T>},
+    }
+);
+
+pub type MockExtrinsic = TestXt<RuntimeCall, ()>;
+
+impl<LocalCall> SendTransactionTypes<LocalCall> for TestRuntime
+where
+    RuntimeCall: From<LocalCall>,
+{
+    type Extrinsic = MockExtrinsic;
+    type OverarchingCall = RuntimeCall;
+}
+
+parameter_types! {
+    pub const GetBaseAssetId: AssetId = XOR;
+    pub const GetBuyBackAssetId: AssetId = XST;
+}
+
+mock_assets_config!(TestRuntime);
+mock_pallet_balances_config!(TestRuntime);
+mock_common_config!(TestRuntime);
+mock_currencies_config!(TestRuntime);
+mock_frame_system_config!(TestRuntime);
+mock_permissions_config!(TestRuntime);
+mock_tokens_config!(TestRuntime);
+mock_technical_config!(TestRuntime);
+
+parameter_types! {
+    pub AdminAccount: AccountId = hex!("8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48").into();
+    pub const CheckInTransferAmount: Balance = 1_000;
+}
+
+impl soratopia::Config for TestRuntime {
+    type RuntimeEvent = RuntimeEvent;
+    type AdminAccount = AdminAccount;
+    type CheckInTransferAmount = CheckInTransferAmount;
+    type WeightInfo = ();
+}
+
+// Builds testing externalities
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    let mut ext: sp_io::TestExternalities = frame_system::GenesisConfig::default()
+        .build_storage::<TestRuntime>()
+        .unwrap()
+        .into();
+    ext.execute_with(|| {
+        System::set_block_number(1); // No events in zero block
+    });
+    ext
+}
