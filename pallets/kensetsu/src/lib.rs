e20cc11524c5a7b3806664b9697511a98cae1ddf
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2024-09-02 21:13:10 +0300

    revert accrue

--- pallets/kensetsu/src/lib.rs
+++ pallets/kensetsu/src/lib.rs
@@ -1838,11 +1838,10 @@ pub mod pallet {
             let stability_fee = interest_coefficient
                 .checked_sub(&cdp.interest_coefficient)
                 .ok_or(Error::<T>::ArithmeticError)?
-                .checked_mul(
-                    &FixedU128::from_inner(cdp.debt)
-                        .checked_div(&cdp.interest_coefficient)
-                        .ok_or(Error::<T>::ArithmeticError)?,
-                )
+                .checked_div(&cdp.interest_coefficient)
+                .ok_or(Error::<T>::ArithmeticError)?;
+            let stability_fee = FixedU128::from_inner(cdp.debt)
+                .checked_mul(&stability_fee)
                 .ok_or(Error::<T>::ArithmeticError)?
                 .into_inner();
             Ok((stability_fee, interest_coefficient))
