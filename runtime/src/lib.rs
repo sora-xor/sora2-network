3fcb623d7e4782e24559f25c738270a20b7cf2b0
Vladimir Stepanenko <vovac12@gmail.com>
2024-09-03 16:03:22 +0700

    Prepare release 3.6.2

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -64,7 +64,7 @@ use bridge_types::types::LeafExtraData;
 use bridge_types::U256;
 use common::prelude::constants::{BIG_FEE, SMALL_FEE};
 use common::prelude::QuoteAmount;
-use common::{AssetId32, Description, PredefinedAssetId};
+use common::{AssetId32, Description, PredefinedAssetId, KUSD};
 use common::{DOT, XOR, XSTUSD};
 use constants::currency::deposit;
 use constants::time::*;
@@ -1022,29 +1022,14 @@ parameter_type_with_key! {
             base_asset_id,
             target_asset_id
         } = trading_pair;
-        (base_asset_id, target_asset_id) == (&XSTUSD.into(), &XOR.into())
+        (base_asset_id, target_asset_id) == (&XSTUSD.into(), &XOR.into()) ||
+        (base_asset_id, target_asset_id) == (&KUSD.into(), &XOR.into())
     };
 }
 
-#[cfg(not(feature = "wip"))] // Chameleon pools
 parameter_type_with_key! {
     pub GetChameleonPools: |base: AssetId| -> Option<(AssetId, sp_std::collections::btree_set::BTreeSet<AssetId>)> {
-        if *base == common::XOR {
-            Some((common::KXOR, [common::ETH].into_iter().collect()))
-        } else {
-            None
-        }
-    };
-}
-
-#[cfg(feature = "wip")] // Chameleon pools
-parameter_type_with_key! {
-    pub GetChameleonPools: |base: AssetId| -> Option<(AssetId, sp_std::collections::btree_set::BTreeSet<AssetId>)> {
-        if *base == common::XOR {
-            Some((common::KXOR, [common::ETH, common::PSWAP, common::VAL].into_iter().collect()))
-        } else {
-            None
-        }
+        None
     };
 }
 
