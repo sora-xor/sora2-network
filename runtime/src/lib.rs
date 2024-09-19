fad7e6332da57db048b22b22407be44807251b67
Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>
2024-09-19 16:22:53 +0300

    Update spec version
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -257,10 +257,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 94,
+    spec_version: 95,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 94,
+    transaction_version: 95,
     state_version: 0,
 };
 
