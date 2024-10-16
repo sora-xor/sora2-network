d8e8f2be3877da967146907997595951b679674b
Vladimir Stepanenko <vovac12@gmail.com>
2024-10-16 20:19:14 +0800

    Prepare release 4.2.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -257,10 +257,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 99,
+    spec_version: 100,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 99,
+    transaction_version: 100,
     state_version: 0,
 };
 
