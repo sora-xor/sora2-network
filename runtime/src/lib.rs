1b2ed3ddcfa7736a182dc5f4c020783f1ee3e7b1
Taras Rusakovich <tarus01@mail.ru>
2024-10-25 14:56:59 +0300

    fex review issues

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -255,10 +255,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 102,
+    spec_version: 101,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 102,
+    transaction_version: 101,
     state_version: 0,
 };
 
