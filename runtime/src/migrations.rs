48ced545bff390b680dafb30bcce5cc5d563c926
Vladimir Stepanenko <vovac12@gmail.com>
2024-12-06 01:14:27 +0700

    Prepare release 4.5.1 (#1287)
    * Prepare release 4.5.1
    
    * Fix Cargo.lock
    
    ---------
    
    Co-authored-by: Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>

--- runtime/src/migrations.rs
+++ runtime/src/migrations.rs
@@ -28,11 +28,7 @@
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-pub type Migrations = (
-    xor_fee::migrations::remove_vxor_remint::Migrate<crate::Runtime>,
-    kensetsu::migrations::v4_to_v5::UpgradeToV5<crate::Runtime>,
-    WipMigrations,
-);
+pub type Migrations = (WipMigrations,);
 
 #[cfg(feature = "wip")]
 pub type WipMigrations =
