57ed7883e579eb9e690280f76b2c4eabe977af17
Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>
2025-01-23 12:10:57 +0300

    Presto KYC (#1309)
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>

--- pallets/xor-fee/src/mock.rs
+++ pallets/xor-fee/src/mock.rs
@@ -486,6 +486,8 @@ impl ExtBuilder {
                     None,
                 ),
             ],
+            regulated_assets: Default::default(),
+            sbt_assets: Default::default(),
         }
         .assimilate_storage(&mut t)
         .unwrap();
