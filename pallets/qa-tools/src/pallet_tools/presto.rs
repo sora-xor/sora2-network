e8a17e2d8c73d2c86eab5efc540f053214825294
Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>
2025-02-11 17:56:15 +0300

    clear presto
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@outlook.com>

--- pallets/qa-tools/src/pallet_tools/presto.rs
+++ pallets/qa-tools/src/pallet_tools/presto.rs
@@ -30,9 +30,10 @@
 
 use crate::Config;
 
+use common::permissions::PermissionId;
 use common::{
-    AccountIdOf, AssetManager, AssetName, AssetSymbol, AssetType, DEXId, DEXInfo, DexIdOf,
-    ExtendedAssetsManager, FromGenericPair, DEFAULT_BALANCE_PRECISION, PRUSD, SBT_PRACS,
+    hash, AccountIdOf, AssetIdOf, AssetManager, AssetName, AssetSymbol, AssetType, DEXId, DEXInfo,
+    DexIdOf, ExtendedAssetsManager, FromGenericPair, DEFAULT_BALANCE_PRECISION, PRUSD, SBT_PRACS,
     SBT_PRCRDT, SBT_PRINVST, XST,
 };
 use frame_support::sp_runtime::{DispatchError, DispatchResult};
@@ -65,7 +66,7 @@ fn presto_buffer_account_id<T: Config>() -> Result<AccountIdOf<T>, DispatchError
     technical::Pallet::<T>::tech_account_id_to_account_id(&tech_account_id)
 }
 
-pub fn register_presto_assets<T: Config>() -> DispatchResult {
+pub fn fill_presto<T: Config>() -> DispatchResult {
     let system_account_id = system_asset_account_id::<T>()?;
     let presto_account_id = presto_main_account_id::<T>()?;
     let presto_buffer_account_id = presto_buffer_account_id::<T>()?;
@@ -99,8 +100,12 @@ pub fn register_presto_assets<T: Config>() -> DispatchResult {
         None,
         None,
     )?;
-    T::ExtendedAssetsManager::set_metadata(&SBT_PRACS.into_predefined().into(), None, now);
-    T::ExtendedAssetsManager::bind_regulated_asset_to_sbt_asset(
+    <T as Config>::ExtendedAssetsManager::set_metadata(
+        &SBT_PRACS.into_predefined().into(),
+        None,
+        now,
+    );
+    <T as Config>::ExtendedAssetsManager::bind_regulated_asset_to_sbt_asset(
         &SBT_PRACS.into_predefined().into(),
         &PRUSD.into(),
     )?;
@@ -130,7 +135,11 @@ pub fn register_presto_assets<T: Config>() -> DispatchResult {
         None,
         None,
     )?;
-    T::ExtendedAssetsManager::set_metadata(&SBT_PRINVST.into_predefined().into(), None, now);
+    <T as Config>::ExtendedAssetsManager::set_metadata(
+        &SBT_PRINVST.into_predefined().into(),
+        None,
+        now,
+    );
 
     T::AssetManager::register_asset_id(
         system_account_id.clone(),
@@ -144,7 +153,11 @@ pub fn register_presto_assets<T: Config>() -> DispatchResult {
         None,
         None,
     )?;
-    T::ExtendedAssetsManager::set_metadata(&SBT_PRCRDT.into_predefined().into(), None, now);
+    <T as Config>::ExtendedAssetsManager::set_metadata(
+        &SBT_PRCRDT.into_predefined().into(),
+        None,
+        now,
+    );
 
     let scopes = [
         Scope::Limited(common::hash(&PRUSD)),
@@ -191,4 +204,127 @@ pub fn register_presto_assets<T: Config>() -> DispatchResult {
     )?;
 
     Ok(())
+}
+
+pub fn clear_presto<T: Config>() -> DispatchResult {
+    let system_account_id = system_asset_account_id::<T>()?;
+    let presto_account_id = presto_main_account_id::<T>()?;
+    let presto_buffer_account_id = presto_buffer_account_id::<T>()?;
+
+    clear_presto_storages::<T>();
+
+    revoke_permission::<T>(
+        &presto_account_id,
+        MANAGE_DEX,
+        Scope::Limited(common::hash(&DEXId::PolkaswapPresto)),
+    );
+
+    let dex_id: DexIdOf<T> = DEXId::PolkaswapPresto.into();
+
+    if dex_manager::DEXInfos::<T>::contains_key(dex_id) {
+        dex_manager::DEXInfos::<T>::remove(dex_id);
+    }
+
+    let scopes = [
+        Scope::Limited(common::hash(&PRUSD)),
+        Scope::Limited(common::hash(&SBT_PRACS)),
+        Scope::Limited(common::hash(&SBT_PRINVST)),
+        Scope::Limited(common::hash(&SBT_PRCRDT)),
+    ];
+
+    let permission_ids = [MINT, BURN];
+
+    for scope in scopes {
+        for permission_id in permission_ids {
+            revoke_permission::<T>(&presto_account_id, permission_id, scope);
+        }
+    }
+
+    extended_assets::SoulboundAsset::<T>::remove::<AssetIdOf<T>>(
+        SBT_PRCRDT.into_predefined().into(),
+    );
+    delete_asset::<T>(SBT_PRCRDT.into_predefined().into(), &system_account_id);
+
+    extended_assets::SoulboundAsset::<T>::remove::<AssetIdOf<T>>(
+        SBT_PRINVST.into_predefined().into(),
+    );
+    delete_asset::<T>(SBT_PRINVST.into_predefined().into(), &system_account_id);
+
+    T::AssetManager::burn_from(
+        &SBT_PRACS.into_predefined().into(),
+        &system_account_id,
+        &presto_buffer_account_id,
+        1,
+    )?;
+    T::AssetManager::burn_from(
+        &SBT_PRACS.into_predefined().into(),
+        &system_account_id,
+        &presto_account_id,
+        1,
+    )?;
+
+    extended_assets::RegulatedAssetToSoulboundAsset::<T>::remove::<AssetIdOf<T>>(PRUSD.into());
+    extended_assets::SoulboundAsset::<T>::remove::<AssetIdOf<T>>(
+        SBT_PRACS.into_predefined().into(),
+    );
+
+    delete_asset::<T>(SBT_PRACS.into_predefined().into(), &system_account_id);
+    delete_asset::<T>(PRUSD.into(), &system_account_id);
+
+    let _ = frame_system::Pallet::<T>::dec_providers(&presto_account_id);
+
+    Ok(())
+}
+
+fn delete_asset<T: Config>(asset_id: T::AssetId, owner: &AccountIdOf<T>) {
+    let permission_ids = [MINT, BURN];
+    for permission_id in permission_ids {
+        revoke_permission::<T>(&owner, permission_id, Scope::Limited(hash(&asset_id)));
+    }
+
+    if assets::AssetInfosV2::<T>::contains_key(asset_id) {
+        assets::AssetInfosV2::<T>::remove(asset_id);
+    }
+    if assets::AssetInfos::<T>::contains_key(asset_id) {
+        assets::AssetInfos::<T>::remove(asset_id);
+    }
+    if assets::AssetOwners::<T>::contains_key(asset_id) {
+        assets::AssetOwners::<T>::remove(asset_id);
+    }
+
+    frame_system::Pallet::<T>::dec_consumers(&owner);
+}
+
+fn revoke_permission<T: Config>(
+    holder: &AccountIdOf<T>,
+    permission_id: PermissionId,
+    scope: Scope,
+) {
+    let mut permissions = permissions::Permissions::<T>::get(holder, scope);
+    if let Ok(id) = permissions.binary_search(&permission_id) {
+        permissions.remove(id);
+
+        if permissions.is_empty() {
+            permissions::Permissions::<T>::remove(holder, scope);
+            if permissions::Permissions::<T>::iter_prefix_values(holder).count() == 0 {
+                frame_system::Pallet::<T>::dec_consumers(holder);
+            }
+        } else {
+            permissions::Permissions::<T>::set(holder, scope, permissions)
+        }
+    }
+}
+
+fn clear_presto_storages<T: Config>() {
+    presto::Managers::<T>::kill();
+    presto::Auditors::<T>::kill();
+    presto::LastCropReceiptId::<T>::kill();
+    let _ = presto::CropReceipts::<T>::clear(u32::MAX, None);
+    let _ = presto::CropReceiptsContent::<T>::clear(u32::MAX, None);
+    let _ = presto::UserCropReceipts::<T>::clear(u32::MAX, None);
+    presto::LastRequestId::<T>::kill();
+    let _ = presto::Requests::<T>::clear(u32::MAX, None);
+    let _ = presto::UserRequests::<T>::clear(u32::MAX, None);
+    presto::LastCouponId::<T>::kill();
+    let _ = presto::Coupons::<T>::clear(u32::MAX, None);
 }
