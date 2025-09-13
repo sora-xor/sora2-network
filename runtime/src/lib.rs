cb4954428582c87629e60ca8c14a1d44c9c403dd
Vladimir Stepanenko <vovac12@gmail.com>
2025-09-13 21:53:13 +0700

    Allow further denomination + prepare release (#1341)
    * Allow further denomination + prepare release
    
    * Fix deps
    
    * Fix clippy

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -258,10 +258,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("sora-substrate"),
     impl_name: create_runtime_str!("sora-substrate"),
     authoring_version: 1,
-    spec_version: 114,
+    spec_version: 115,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 114,
+    transaction_version: 115,
     state_version: 0,
 };
 
