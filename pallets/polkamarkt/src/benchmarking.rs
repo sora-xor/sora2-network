4f8e409fbb067e7110fa2bed08fd86c1c40eb446
William Richter <richter@soramitsu.co.jp>
2026-02-27 10:32:21 +0100

    Fix Polkamarkt benchmarking return types

--- pallets/polkamarkt/src/benchmarking.rs
+++ pallets/polkamarkt/src/benchmarking.rs
@@ -2,7 +2,6 @@
 
 use super::*;
 use frame_benchmarking::v2::*;
-use frame_benchmarking::BenchmarkError;
 use frame_support::traits::Get;
 use frame_system::RawOrigin;
 use sp_runtime::traits::{One, Zero};
@@ -26,22 +25,22 @@ mod benchmarks {
     use super::*;
 
     #[benchmark]
-    fn create_condition() -> Result<(), BenchmarkError> {
+    fn create_condition() {
         let caller: T::AccountId = whitelisted_caller();
         GovernanceBonds::<T>::insert(&caller, T::GovernanceBondMinimum::get());
         let metadata = default_condition_input::<BlockNumberFor<T>>();
 
         #[extrinsic_call]
         create_condition(RawOrigin::Signed(caller), metadata);
-        Ok(())
     }
 
     #[benchmark]
-    fn create_market() -> Result<(), BenchmarkError> {
+    fn create_market() {
         let caller: T::AccountId = whitelisted_caller();
         GovernanceBonds::<T>::insert(&caller, T::GovernanceBondMinimum::get());
         let metadata = default_condition_input::<BlockNumberFor<T>>();
-        Pallet::<T>::create_condition(RawOrigin::Signed(caller.clone()).into(), metadata)?;
+        Pallet::<T>::create_condition(RawOrigin::Signed(caller.clone()).into(), metadata)
+            .expect("condition setup");
 
         let close = <frame_system::Pallet<T>>::block_number()
             + T::MinMarketDuration::get()
@@ -55,15 +54,15 @@ mod benchmarks {
             T::Balance::zero(),
             None,
         );
-        Ok(())
     }
 
     #[benchmark]
-    fn commit_order() -> Result<(), BenchmarkError> {
+    fn commit_order() {
         let caller: T::AccountId = whitelisted_caller();
         GovernanceBonds::<T>::insert(&caller, T::GovernanceBondMinimum::get());
         let metadata = default_condition_input::<BlockNumberFor<T>>();
-        Pallet::<T>::create_condition(RawOrigin::Signed(caller.clone()).into(), metadata)?;
+        Pallet::<T>::create_condition(RawOrigin::Signed(caller.clone()).into(), metadata)
+            .expect("condition setup");
         let close = <frame_system::Pallet<T>>::block_number()
             + T::MinMarketDuration::get()
             + BlockNumberFor::<T>::one();
@@ -73,20 +72,21 @@ mod benchmarks {
             close,
             T::Balance::zero(),
             None,
-        )?;
+        )
+        .expect("market setup");
 
         let commitment = [1u8; 32];
 
         #[extrinsic_call]
         commit_order(RawOrigin::Signed(caller), 0, commitment);
-        Ok(())
     }
 
     #[benchmark]
-    fn reveal_order() -> Result<(), BenchmarkError> {
+    fn reveal_order() {
         let caller: T::AccountId = whitelisted_caller();
         let metadata = default_condition_input::<BlockNumberFor<T>>();
-        Pallet::<T>::create_condition(RawOrigin::Signed(caller.clone()).into(), metadata)?;
+        Pallet::<T>::create_condition(RawOrigin::Signed(caller.clone()).into(), metadata)
+            .expect("condition setup");
         let close = <frame_system::Pallet<T>>::block_number()
             + T::MinMarketDuration::get()
             + BlockNumberFor::<T>::one();
@@ -96,13 +96,15 @@ mod benchmarks {
             close,
             T::Balance::zero(),
             None,
-        )?;
+        )
+        .expect("market setup");
 
         let payload = b"BUY:10@50".to_vec();
         let salt = b"benchmark".to_vec();
         let commitment = Pallet::<T>::compute_commitment_hash(&caller, 0, &payload, &salt);
 
-        Pallet::<T>::commit_order(RawOrigin::Signed(caller.clone()).into(), 0, commitment)?;
+        Pallet::<T>::commit_order(RawOrigin::Signed(caller.clone()).into(), 0, commitment)
+            .expect("commit order");
 
         let now = <frame_system::Pallet<T>>::block_number()
             + T::CommitmentRevealDelay::get()
@@ -117,7 +119,6 @@ mod benchmarks {
             salt,
             T::Balance::one(),
         );
-        Ok(())
     }
 
     #[cfg(test)]
