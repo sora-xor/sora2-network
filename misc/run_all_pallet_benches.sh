7095d0a067cfc9865708190f20ee6fd67fc3518d
武宮　誠 <t@soramitsu.co.jp>
2026-04-07 20:40:16 +1100

    review

--- misc/run_all_pallet_benches.sh
+++ misc/run_all_pallet_benches.sh
@@ -5,5 +5,5 @@ source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/benchmark_helpers.sh
 
 REFERENCE_BINARY="/usr/local/bin/framenode"
 
-benchmark::require_benchmark_binary "${REFERENCE_BINARY}"
-benchmark::run_all_pallet_targets "${REFERENCE_BINARY}"
+benchmark::require_chain_benchmark_binary "${REFERENCE_BINARY}"
+benchmark::run_all_pallet_targets "${REFERENCE_BINARY}" benchmark::chain_args "local chain"
